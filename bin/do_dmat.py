43fb670e7a57cb5b0e7ff1eead2868fb50a1c157
Victoria Serret <vserret@lpnhe.in2p3.fr>
2017-07-07 18:14:49 +0200

    extention for rp negative

--- bin/do_dmat.py
+++ bin/do_dmat.py
@@ -44,6 +44,9 @@ if __name__ == '__main__':
     parser.add_argument('--rt-max', type = float, default = 200, required=False,
                         help = 'max rt')
 
+    parser.add_argument('--rp-min', type = float, default = 0, required=False,
+                        help = 'min rp. rp can be <0')
+    
     parser.add_argument('--np', type = int, default = 50, required=False,
                         help = 'number of r-parallel bins')
 
@@ -89,6 +92,7 @@ if __name__ == '__main__':
 
     cf.rp_max = args.rp_max
     cf.rt_max = args.rt_max
+    cf.rp_min = args.rp_min 
     cf.np = args.np
     cf.nt = args.nt
     cf.nside = args.nside
