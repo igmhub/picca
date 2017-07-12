c7661632734756109c18b3bb30276c8d805e982d
Hélion du Mas des Bourboux <helion.du-mas-des-bourboux@cea.fr>
2017-07-12 17:55:08 +0200

    fix_issue_157

--- bin/do_xMetalGrid.py
+++ bin/do_xMetalGrid.py
@@ -36,13 +36,13 @@ if __name__ == '__main__':
                         help = 'drq')
 
     parser.add_argument('--rp-max', type = float, default = 200, required=False,
-                        help = 'max rp')
+                        help = 'max rp [h^-1 Mpc]')
 
     parser.add_argument('--rp-min', type = float, default = -200, required=False,
-                        help = 'max rp')
+                        help = 'min rp [h^-1 Mpc]')
 
     parser.add_argument('--rt-max', type = float, default = 200, required=False,
-                        help = 'max rt')
+                        help = 'max rt [h^-1 Mpc]')
 
     parser.add_argument('--np', type = int, default = 100, required=False,
                         help = 'number of r-parallel bins')
@@ -51,10 +51,10 @@ if __name__ == '__main__':
                         help = 'number of r-transverse bins')
 
     parser.add_argument('--lambda-abs', type = float, default = constants.absorber_IGM["LYA"], required=False,
-                        help = 'wavelength of absorption')
+                        help = 'wavelength of absorption [Angstrom]')
 
     parser.add_argument('--lambda-abs-metal', type = float, default = constants.absorber_IGM["LYA"], required=False,
-                        help = 'wavelength of absorption of metal')
+                        help = 'wavelength of absorption of metal [Angstrom]')
 
     parser.add_argument('--fid-Om', type = float, default = 0.315, required=False,
                     help = 'Om of fiducial cosmology')
