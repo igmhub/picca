8fe3847d462a71ce03166da690e25a0e196cad83
ngbusca <ngbusca@apc.in2p3.fr>
2017-10-21 02:06:46 -0600

    added eta, vlss fit to the output

--- py/picca/prep_del.py
+++ py/picca/prep_del.py
@@ -26,21 +26,23 @@ def mc(data):
     return ll,mcont,wcont
 
 def var_lss(data,eta_lim=(0.5,1.5),vlss_lim=(0.,0.3)):
-    nlss = 10
+    nlss = 20
     eta = sp.zeros(nlss)
     vlss = sp.zeros(nlss)
+    fudge = sp.zeros(nlss)
     nb_pixels = sp.zeros(nlss)
     ll = forest.lmin + (sp.arange(nlss)+.5)*(forest.lmax-forest.lmin)/nlss
 
     nwe = 100
     vpmin = sp.log10(1e-5)
     vpmax = sp.log10(2.)
-
     var = 10**(vpmin + (sp.arange(nwe)+.5)*(vpmax-vpmin)/nwe)
+
     var_del =sp.zeros(nlss*nwe)
     mdel =sp.zeros(nlss*nwe)
     var2_del =sp.zeros(nlss*nwe)
     count =sp.zeros(nlss*nwe)
+    nqso = sp.zeros(nlss*nwe)
 
     for p in data:
         for d in data[p]:
@@ -70,6 +72,8 @@ def var_lss(data,eta_lim=(0.5,1.5),vlss_lim=(0.,0.3)):
 
             c = sp.bincount(bins)
             count[:len(c)] += c
+            nqso[sp.unique(bins)]+=1
+
     
     w = count>0
     var_del[w]/=count[w]
@@ -79,23 +83,26 @@ def var_lss(data,eta_lim=(0.5,1.5),vlss_lim=(0.,0.3)):
     var2_del -= var_del**2
     var2_del[w]/=count[w]
 
+    bin_chi2 = sp.zeros(nlss)
     for i in range(nlss):
-        def chi2(eta,vlss):
-            v = var_del[i*nwe:(i+1)*nwe]-eta*var-vlss
+        def chi2(eta,vlss,fudge):
+            v = var_del[i*nwe:(i+1)*nwe]-eta*var-vlss-fudge/var
             dv2 = var2_del[i*nwe:(i+1)*nwe]
             n = count[i*nwe:(i+1)*nwe]
-            w=(dv2>0) & (n>100)
+            w=nqso[i*nwe:(i+1)*nwe]>100
             return sp.sum(v[w]**2/dv2[w])
-        mig = iminuit.Minuit(chi2,forced_parameters=("eta","vlss"),eta=1.,vlss=0.1,error_eta=0.05,error_vlss=0.05,errordef=1.,print_level=0,limit_eta=eta_lim,limit_vlss=vlss_lim)
+        mig = iminuit.Minuit(chi2,forced_parameters=("eta","vlss","fudge"),eta=1.,vlss=0.1,fudge=0.05,error_eta=0.05,error_vlss=0.05,error_fudge=0.05,errordef=1.,print_level=0,limit_eta=eta_lim,limit_vlss=vlss_lim, limit_fudge=(0,None))
         mig.migrad()
 
         eta[i] = mig.values["eta"]
         vlss[i] = mig.values["vlss"]
+        fudge[i] = mig.values["fudge"]
         nb_pixels[i] = count[i*nwe:(i+1)*nwe].sum()
-        print eta[i],vlss[i],mig.fval, nb_pixels[i]
+        bin_chi2[i] = mig.fval
+        print eta[i],vlss[i],fudge[i],mig.fval, nb_pixels[i]
 
 
-    return ll,eta,vlss,nb_pixels
+    return ll,eta,vlss,fudge,nb_pixels,var,var_del.reshape(nlss,-1),var2_del.reshape(nlss,-1),count.reshape(nlss,-1),nqso.reshape(nlss,-1),bin_chi2
 
     
 def stack(data,delta=False):
