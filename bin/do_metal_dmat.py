91e4f9b4614d6dc4bc0010383b47b88d59aece53
ngbusca <ngbusca@apc.in2p3.fr>
2017-11-14 06:10:11 -0700

    refactored do_metal_xdmat

--- bin/do_metal_dmat.py
+++ bin/do_metal_dmat.py
@@ -49,11 +49,11 @@ if __name__ == '__main__':
     parser.add_argument('--nt', type = int, default = 50, required=False,
                         help = 'number of r-transverse bins')
 
-    parser.add_argument('--lambda-abs', type = float, default = constants.absorber_IGM['LYA'], required=False,
-                        help = 'wavelength of absorption [Angstrom]')
+    parser.add_argument('--lambda-abs', type = str, default = 'LYA', required=False,
+                        help = 'name of the absorption wavelength')
 
-    parser.add_argument('--lambda-abs2', type = float, default = constants.absorber_IGM['LYA'], required=False,
-                        help = 'wavelength of absorption #2 [Angstrom]')
+    parser.add_argument('--lambda-abs2', type = str, default = None, required=False,
+                        help = 'name of absorption #2')
 
     parser.add_argument('--fid-Om', type = float, default = 0.315, required=False,
                     help = 'Om of fiducial cosmology')
@@ -105,8 +105,12 @@ if __name__ == '__main__':
     cf.nside = args.nside
     cf.zref = args.z_ref
     cf.alpha = args.z_evol
-    cf.lambda_abs = args.lambda_abs
-    cf.lambda_abs2 = args.lambda_abs2
+    cf.lambda_abs = constants.absorber_IGM[args.lambda_abs]
+    if args.lambda_abs2 is not None:
+        cf.lambda_abs2 = constants.absorber_IGM[args.lambda_abs2]
+    else:
+        cf.lambda_abs2 = constants.absorber_IGM[args.lambda_abs]
+
     cf.rej = args.rej
 
     cosmo = constants.cosmo(args.fid_Om)
@@ -130,7 +134,7 @@ if __name__ == '__main__':
     sys.stderr.write("read {}\n".format(ndata))
 
     x_correlation=False
-    if args.in_dir2: 
+    if args.in_dir2 is not None: 
         x_correlation=True
         ndata2 = 0
         fi = glob.glob(args.in_dir2+"/*.fits.gz")
@@ -151,9 +155,10 @@ if __name__ == '__main__':
         data2  = copy.deepcopy(data)
         ndata2 = copy.deepcopy(ndata)
         dels2  = copy.deepcopy(dels)
+
     cf.x_correlation=x_correlation 
     
-    z_min_pix = 10**dels[0].ll[0]/args.lambda_abs-1.
+    z_min_pix = 10**dels[0].ll[0]/cf.lambda_abs-1.
     phi = [d.ra for d in dels]
     th = [sp.pi/2.-d.dec for d in dels]
     pix = healpy.ang2pix(cf.nside,th,phi)
@@ -162,7 +167,7 @@ if __name__ == '__main__':
             data[p]=[]
         data[p].append(d)
 
-        z = 10**d.ll/args.lambda_abs-1.
+        z = 10**d.ll/cf.lambda_abs-1.
         z_min_pix = sp.amin( sp.append([z_min_pix],z) )
         d.z = z
         d.r_comov = cosmo.r_comoving(z)
@@ -170,7 +175,7 @@ if __name__ == '__main__':
 
 
     if x_correlation: 
-        z_min_pix2 = 10**dels2[0].ll[0]/args.lambda_abs2-1.
+        z_min_pix2 = 10**dels2[0].ll[0]/cf.lambda_abs2-1.
         z_min_pix=sp.amin(sp.append(z_min_pix,z_min_pix2))
         phi2 = [d.ra for d in dels2]
         th2 = [sp.pi/2.-d.dec for d in dels2]
@@ -181,7 +186,7 @@ if __name__ == '__main__':
                 data2[p]=[]
             data2[p].append(d)
 
-            z = 10**d.ll/args.lambda_abs2-1.
+            z = 10**d.ll/cf.lambda_abs2-1.
             z_min_pix2 = sp.amin(sp.append([z_min_pix2],z) )
             d.z = z
             d.r_comov = cosmo.r_comoving(z)
@@ -214,6 +219,8 @@ if __name__ == '__main__':
 
     random.seed(0)
 
+    abs_igm = [args.lambda_abs]+args.abs_igm
+
     for i,p in enumerate(cpu_data.values()):
         print "filling neighs ",i,len(cpu_data.values())
         if x_correlation: 
@@ -230,32 +237,19 @@ if __name__ == '__main__':
     npairs_all=[]
     npairs_used_all=[]
 
-    if args.lambda_abs == constants.absorber_IGM['LYA']: 
-        abs_igm = ["LYA"]+args.abs_igm
-    elif args.lambda_abs == constants.absorber_IGM['LYB']:
-        abs_igm = ["LYB"]+args.abs_igm
-    else:
-        print("ERROR: abs_igm is not known")
-        sys.exit(12)
-
     print("abs_igm = {}".format(abs_igm))
 
     if args.abs_igm2: 
         print "args.lambda_abs2 = ", args.lambda_abs2
-        if args.lambda_abs2 == constants.absorber_IGM['LYA']: 
-            abs_igm_2 = ["LYA"]+args.abs_igm2
-        elif args.lambda_abs2 == constants.absorber_IGM['LYB']:
-            abs_igm_2 = ["LYB"]+args.abs_igm2 
-        else: 
-            print("ERROR: abs_igm_2 is not known")
-            sys.exit(12)
+        abs_igm_2 = [args.lambda_abs2]+args.abs_igm2
     else: 
-        abs_igm_2=copy.deepcopy(abs_igm)
+        abs_igm_2 = abs_igm
+
     print("abs_igm_2 = {}".format(abs_igm_2))
    
     for i,abs_igm1 in enumerate(abs_igm):
         i0=i
-        if cf.lambda_abs != cf.lambda_abs2: i0=0
+        if args.lambda_abs != args.lambda_abs2: i0=0
         for j in range(i0,len(abs_igm_2)):
             if ((i==0)and(j==0)): continue 
             abs_igm2 = abs_igm_2[j]
@@ -264,6 +258,7 @@ if __name__ == '__main__':
             sys.stderr.write("\n")
             pool = Pool(processes=args.nproc)
             dm = pool.map(f,cpu_data.values())
+            #dm = map(f,cpu_data.values())
             pool.close()
             dm = sp.array(dm)
             wdm =dm[:,0].sum(axis=0)
