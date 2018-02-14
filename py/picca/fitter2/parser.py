27388767520816b5f7e208b930d9607b08184756
londumas <helion331990@gmail.com>
2018-02-14 13:23:13 -0700

    reads Pk by default if not given

--- py/picca/fitter2/parser.py
+++ py/picca/fitter2/parser.py
@@ -21,7 +21,17 @@ def parse_chi2(filename):
     dic_init['data sets'] = [data.data(parse_data(os.path.expandvars(d))) for d in cp.get('data sets','ini files').split()]
 
     dic_init['fiducial'] = {}
-    h = fitsio.FITS(cp.get('fiducial','filename'))
+
+    if cp.has_section('fiducial'):
+        p = cp.get('fiducial','filename')
+        p = os.path.expandvars(p)
+        print('INFO: reading input Pk {}'.format(p))
+    else:
+        p = '$PICCA_BASE/py/picca/fitter2/models/PlanckDR12/PlanckDR12.fits'
+        p = os.path.expandvars(p)
+        print('INFO: reading default Pk {}'.format(p))
+
+    h = fitsio.FITS(p)
     dic_init['fiducial']['k'] = h[1]['K'][:]
     dic_init['fiducial']['pk'] = h[1]['PK'][:]
     dic_init['fiducial']['pksb'] = h[1]['PKSB'][:]
