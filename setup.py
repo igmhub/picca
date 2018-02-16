8bd7baf48150e4681865f738cd6f284736a53527
ngbusca <ngbusca@in2p3.fr>
2018-02-15 23:57:31 -0800

    tweaked setup to add dependencies

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+
+import glob
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+scripts = glob.glob('bin/*')
 
 description = "Package for Igm Cosmological-Correlations Analyses"
 
@@ -12,10 +17,11 @@ setup(name="picca",
       description=description,
       url="https://github.com/igmhub/picca",
       author="Nicolas Busca et al",
-      author_email="ngbusca@apc.in2p3.fr",
+      author_email="ngbusca@lpnhe.in2p3.fr",
       packages=['picca'],
       package_dir = {'': 'py'},
       install_requires=['scipy','numpy','iminuit','fitsio','healpy','numba','future'],
-      test_suite='picca.test.test_cor'
+      test_suite='picca.test.test_cor',
+      scripts = scripts
       )
 
