a6ed1bdaa63aac1cbd2e30410ddc8be771531007
VictoriaSerret <vserret@lpnhe.in2p3.fr>
2018-04-23 16:11:02 +0200

    apply codacy recommendations

--- py/picca/fitter2/chi2.py
+++ py/picca/fitter2/chi2.py
@@ -269,6 +269,7 @@ class chi2:
                 d[...] = vals[:,0]
                 d = g.create_dataset("{}/errors".format(p), vals[:,1].shape, dtype="f")
                 d[...] = vals[:,1]
+                
             for p in self.fast_mc_data: 
                 xi = self.fast_mc_data[p] 
                 d = g.create_dataset(p, xi.shape, dtype="f")
