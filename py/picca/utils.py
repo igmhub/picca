8ffb64fb58b614a5eac980919b7e4965342be0bf
londumas <helion331990@gmail.com>
2018-05-29 09:43:00 -0700

    get transmission files diferently

--- py/picca/utils.py
+++ py/picca/utils.py
@@ -186,7 +186,7 @@ def desi_from_ztarget_to_drq(ztarget,drq,spectype='QSO',downsampling_z_cut=None,
     out.close()
 
     return
-def desi_convert_transmission_to_delta_files(zcat,indir,outdir,lObs_min=3600.,lObs_max=5500.,lRF_min=1040.,lRF_max=1200.,dll=3.e-4,nspec=None):
+def desi_convert_transmission_to_delta_files(zcat,outdir,indir=None,infiles=None,lObs_min=3600.,lObs_max=5500.,lRF_min=1040.,lRF_max=1200.,dll=3.e-4,nspec=None):
     """Convert desi transmission files to picca delta files
 
     Args:
@@ -214,17 +214,24 @@ def desi_convert_transmission_to_delta_files(zcat,indir,outdir,lObs_min=3600.,lO
         zcat_thid = h[1]['THING_ID'][:]
     w = h[1]['Z'][:]>max(0.,lObs_min/lRF_max -1.)
     w &= h[1]['Z'][:]<max(0.,lObs_max/lRF_min -1.)
+    zcat_ra = h[1]['RA'][:][w].astype('float64')*sp.pi/180.
+    zcat_dec = h[1]['DEC'][:][w].astype('float64')*sp.pi/180.
     zcat_thid = zcat_thid[w]
     h.close()
 
     ### List of transmission files
-    if len(indir)>8 and indir[-8:]=='.fits.gz':
-        fi = glob.glob(indir)
-    elif len(indir)>5 and indir[-5:]=='.fits':
-        fi = glob.glob(indir)
+    if (indir is None and infiles is None) or (indir is not None and infiles is not None):
+        print("ERROR: No transmisson input files or both 'indir' and 'infiles' given")
+        sys.exit()
+    elif indir is not None:
+        in_nside = int(in_dir.split('spectra-')[-1].replace('/',''))
+        nest = True
+        in_pixs = healpy.ang2pix(in_nside, sp.pi/2.-zcat_dec, zcat_ra, nest=nest)
+        fi = sp.unique(in_pixs)
+        fi = [ indir+'/'+str(int(f/100))+'/'+str(f)+'/spectra-'+str(in_nside)+'-'+str(f)+'.fits' for f in fi]
+        fi = sp.sort(sp.array(fi))
     else:
-        fi = glob.glob(indir+'/*.fits') + glob.glob(indir+'/*.fits.gz')
-    fi = sp.sort(sp.array(fi))
+        fi = sp.sort(sp.array(infiles))
 
     ### Stack the transmission
     lmin = sp.log10(lObs_min)
--- tutorials/desi/do_convert_transmission.py
+++ tutorials/desi/do_convert_transmission.py
@@ -10,35 +10,38 @@ if __name__ == '__main__':
             description='Script to convert noiseless transmission files to delta picca files')
 
     parser.add_argument('--object-cat', type = str, default = None, required=True,
-            help = 'path to a catalog of objects to get the transmission from')
+            help = 'Path to a catalog of objects to get the transmission from')
 
-    parser.add_argument('--in-dir',type = str,default=None,required=True,
-            help='data directory')
+    parser.add_argument('--in-dir',type = str,default=None,required=False,
+            help='Desi formated data directory to transmission files')
+
+    parser.add_argument('--in-files',type = str,default=None,required=False,
+            help='List of transmission files', nargs='*')
 
     parser.add_argument('--out-dir',type = str,default=None,required=True,
-            help='output directory')
+            help='Output directory')
 
     parser.add_argument('--lambda-min',type = float,default=3600.,required=False,
-            help='lower limit on observed wavelength [Angstrom]')
+            help='Lower limit on observed wavelength [Angstrom]')
 
     parser.add_argument('--lambda-max',type = float,default=5500.,required=False,
-            help='upper limit on observed wavelength [Angstrom]')
+            help='Upper limit on observed wavelength [Angstrom]')
 
     parser.add_argument('--lambda-rest-min',type = float,default=1040.,required=False,
-            help='lower limit on rest frame wavelength [Angstrom]')
+            help='Lower limit on rest frame wavelength [Angstrom]')
 
     parser.add_argument('--lambda-rest-max',type = float,default=1200.,required=False,
-            help='upper limit on rest frame wavelength [Angstrom]')
+            help='Upper limit on rest frame wavelength [Angstrom]')
 
     parser.add_argument('--dll',type = float,default=3.e-4,required=False,
             help='Size of the rebined pixels in log lambda')
 
     parser.add_argument('--nspec',type = int,default=None,required=False,
-            help="number of spectra to fit, if None then run on all files")
+            help="Number of spectra to fit, if None then run on all files")
 
     args = parser.parse_args()
 
-    utils.desi_convert_transmission_to_delta_files(zcat = args.object_cat, indir = args.in_dir, outdir = args.out_dir,
+    utils.desi_convert_transmission_to_delta_files(zcat = args.object_cat, outdir = args.out_dir, indir = args.in_dir, infiles = args.in_files,
         lObs_min = args.lambda_min, lObs_max = args.lambda_max,
         lRF_min = args.lambda_rest_min, lRF_max = args.lambda_rest_max,
         dll = args.dll, nspec = args.nspec)
