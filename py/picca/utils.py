7a91a3422d74990c353cfe2d9f5339d7afc07b70
londumas <helion331990@gmail.com>
2018-12-19 05:25:53 -0700

    Shuffle distribution objects

--- py/picca/utils.py
+++ py/picca/utils.py
@@ -366,3 +366,21 @@ def compute_ang_max(cosmo,rt_max,zmin,zmin2=None):
         angmax = 2.*sp.arcsin(rt_max/(rmin1+rmin2))
 
     return angmax
+def shuffle_distrib_obj(obj,seed):
+
+    dic = {}
+    lst_p = ['we','zqso','r_comov']
+    for p in lst_p:
+        dic[p] = [getattr(o, p) for oss in obj.values() for o in oss]
+
+    sp.random.seed(seed)
+    idx = sp.arange(len(dic['zqso']))
+    sp.random.shuffle(idx)
+
+    i = 0
+    for oss in obj.values():
+        for o in oss:
+            for p in lst_p:
+                setattr(o,p,dic[p][idx[i]])
+            i += 1
+    return obj
