fa02400d186d1232b9966c4b4d270f43cd9183a3
philippedavid <39967130+philippedavid@users.noreply.github.com>
2019-02-12 14:05:40 -0700

    read_dust_map now returns true EBV value
    EBV = Rv * E(B-V), where Rv = 3.793 in the catalogue

--- py/picca/io.py
+++ py/picca/io.py
@@ -142,10 +142,10 @@ def read_drq(drq,zmin,zmax,keep_bal,bi_max=None):
 
     return ra,dec,zqso,thid,plate,mjd,fid
 
-def read_dust_map(drq):
+def read_dust_map(drq, Rv = 3.793):
     h = fitsio.FITS(drq)
     thid = h[1]['THING_ID'][:]
-    ext  = h[1]['EXTINCTION'][:][:,1]
+    ext  = h[1]['EXTINCTION'][:][:,1]/3.793
     h.close()
 
     return dict(zip(thid, ext))
