fb8e28bce464727025cb52d7dd1e73a24bad4e80
Edmond <47702632+echaussidon@users.noreply.github.com>
2019-08-06 11:38:39 +0200

    mini update

--- py/picca/fitter2/data.py
+++ py/picca/fitter2/data.py
@@ -315,7 +315,8 @@ class data:
             hmet.close()
 
     def xi_model(self, k, pk_lin, pars):
-        xi = self.xi(self.r, self.mu, k, pk_lin, self.pk, tracer1 = self.tracer1, tracer2 = self.tracer2, ell_max = self.ell_max, **pars)
+        xi = self.xi(self.r, self.mu, k, pk_lin, self.pk, \
+                     tracer1 = self.tracer1, tracer2 = self.tracer2, ell_max = self.ell_max, **pars)
 
         xi *= self.z_evol[self.tracer1['name']](self.z, self.tracer1, **pars)*self.z_evol[self.tracer2['name']](self.z, self.tracer2, **pars)
         xi *= self.growth_function(self.z, **pars)**2
