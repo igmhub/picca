522a63847e4576048ef6dafeb39c1080d9e096db
James Farr <james.a.farr@btopenworld.com>
2019-08-06 15:10:32 +0100

    First attempt at including shuffled correlation removal

--- bin/picca_export_coadd_zint.py
+++ bin/picca_export_coadd_zint.py
@@ -20,27 +20,72 @@ parser.add_argument("--out",type=str,required=True,
 parser.add_argument("--no-dmat",action='store_true',default=False,required=False,
         help='Use an identity matrix as the distortion matrix.')
 
+parser.add_argument('--remove-shuffled-correlation',type=str,default=None,nargs="*",required=False,
+        help="the shuffled (x)cf_z_....fits files to be coadded and then removed")
+
 args=parser.parse_args()
 
 for f in args.data:
     if not os.path.isfile(f):
         args.data.remove(f)
 
-h=fitsio.FITS(args.data[0])
+# Function to coadd a set of cf of xcf measurements.
+def coadd_correlations(fi):
+    # Define variables of the correct shape to store correlation information.
+    h = fitsio.FITS(fi[0])
+    head = h[1].read_header()
+    rp = h[1]['RP'][:]*0
+    rt = h[1]['RT'][:]*0
+    nb = h[1]['NB'][:]*0
+    z = h[1]['Z'][:]*0
+    wet = rp*0
+    h.close()
+    da = {}
+    we = {}
 
-head = h[1].read_header()
-rp = h[1]['RP'][:]*0
-rt = h[1]['RT'][:]*0
-nb = h[1]['NB'][:]*0
-z = h[1]['Z'][:]*0
-wet = rp*0
-h.close()
+    # For each data file:
+    for f in fi:
+        if not (os.path.isfile(f.replace('cf','dmat')) or args.no_dmat):
+            continue
 
-da = {}
-we = {}
+        # Add information about the weights and correlation bins.
+        h = fitsio.FITS(f)
+        we_aux = h[2]["WE"][:]
+        wet_aux = we_aux.sum(axis=0)
+        rp += h[1]['RP'][:]*wet_aux
+        rt += h[1]['RT'][:]*wet_aux
+        z  += h[1]['Z'][:]*wet_aux
+        nb += h[1]['NB'][:]
+        wet += wet_aux
 
-if not args.no_dmat:
-    h = fitsio.FITS(args.data[0].replace('cf','dmat'))
+        # Add to the data and weights dictionaries.
+        hid = h[2]['HEALPID'][:]
+        for i,p in enumerate(hid):
+            print("\rcoadding healpix {} in file {}".format(p,f),end="")
+            if p in da:
+                da[p] += h[2]["DA"][:][i]*we_aux[i]
+                we[p] += we_aux[i,:]
+            else:
+                da[p] = h[2]["DA"][:][i]*we_aux[i]
+                we[p] = we_aux[i]
+        h.close()
+
+
+    # Normalise all variables by the total weights.
+    for p in da:
+        w = we[p]>0
+        da[p][w] /= we[p][w]
+    rp /= wet
+    rt /= wet
+    z /= wet
+
+    return rp,rt,nb,z,wet,da,we,dm,head
+
+# Function to coadd a set of cf of xcf distortion matrices.
+def coadd_dmats(fi):
+
+    #If there are distortion matrices, set up variables of the correct shapes.
+    h = fitsio.FITS(fi[0].replace('cf','dmat'))
     dm = h[1]['DM'][:]*0
     try:
         dmrp = sp.zeros(h[2]['RP'][:].size)
@@ -50,33 +95,11 @@ if not args.no_dmat:
     except IOError:
         pass
     h.close()
-else:
-    dm = sp.eye(nb.shape[0])
 
-for f in args.data:
-    if not (os.path.isfile(f.replace('cf','dmat')) or args.no_dmat):
-        continue
-    h = fitsio.FITS(f)
-    we_aux = h[2]["WE"][:]
-    wet_aux = we_aux.sum(axis=0)
-    rp += h[1]['RP'][:]*wet_aux
-    rt += h[1]['RT'][:]*wet_aux
-    z  += h[1]['Z'][:]*wet_aux
-    nb += h[1]['NB'][:]
-    wet += wet_aux
-
-    hid = h[2]['HEALPID'][:]
-    for i,p in enumerate(hid):
-        print("\rcoadding healpix {} in file {}".format(p,f),end="")
-        if p in da:
-            da[p] += h[2]["DA"][:][i]*we_aux[i]
-            we[p] += we_aux[i,:]
-        else:
-            da[p] = h[2]["DA"][:][i]*we_aux[i]
-            we[p] = we_aux[i]
-    h.close()
-
-    if not args.no_dmat:
+    for f in fi:
+        if not (os.path.isfile(f.replace('cf','dmat')) or args.no_dmat):
+            continue
+        # If there are distortion matrices, add in contributions.
         h = fitsio.FITS(f.replace('cf','dmat'))
         dm += h[1]['DM'][:]*wet_aux[:,None]
         if 'dmrp' in locals():
@@ -87,39 +110,55 @@ for f in args.data:
             nbdm += 1.
         h.close()
 
-for p in da:
-    w=we[p]>0
-    da[p][w]/=we[p][w]
-
-rp /= wet
-rt /= wet
-z /= wet
-if not args.no_dmat:
+    # Normalise all variables by the total weights.
     dm /= wet[:,None]
+    if 'dmrp' in locals():
+        dmrp /= nbdm
+        dmrt /= nbdm
+        dmz /= nbdm
 
+    return dmrp,dmrt,dmz,nbdm,dm
+
+# Coadd the data files.
+rp,rt,nb,z,wet,da,we,head = coadd_correlations(args.data)
+
+# If required, coadd the distortion matrices.
+if not args.no_dmat:
+    dmrp,dmrt,dmz,nbdm,dm = coadd_dmats(args.data)
+else:
+    dm = sp.eye(nb.shape[0])
+
+# If required, remove the shuffled correlations.
+if not args.remove_shuffled_correlation is None:
+    rp_s,rt_s,nb_s,z_s,wet_s,da_s,we_s,dm_s,head_s = coadd_correlations(args.remove_shuffled_correlation)
+    da_s = (da_s*we_s).sum(axis=1)
+    we_s = we_s.sum(axis=1)
+    w = we_s>0.
+    da_s[w] /= we_s[w]
+    da -= da_s[:,None]
+
+# Average data and weights over all HEALPix pixels.
 da = sp.vstack(list(da.values()))
 we = sp.vstack(list(we.values()))
-
-co = smooth_cov(da,we,rp,rt)
 da = (da*we).sum(axis=0)
 da /= wet
 
+# Calculate the smoothed covariance from subsampling.
+co = smooth_cov(da,we,rp,rt)
 
-if 'dmrp' in locals():
-    dmrp /= nbdm
-    dmrt /= nbdm
-    dmz /= nbdm
+# Not sure what this does.
 if ('dmrp' not in locals()) or (dmrp.size==rp.size):
     dmrp = rp.copy()
     dmrt = rt.copy()
     dmz = z.copy()
 
+# Check to see if the covariance is positive definite.
 try:
     scipy.linalg.cholesky(co)
 except scipy.linalg.LinAlgError:
     print('WARNING: Matrix is not positive definite')
 
-
+# Write the exported correlation file.
 h = fitsio.FITS(args.out,"rw",clobber=True)
 comment = ['R-parallel','R-transverse','Redshift','Correlation','Covariance matrix','Distortion matrix','Number of pairs']
 h.write([rp,rt,z,da,co,dm,nb],names=['RP','RT','Z','DA','CO','DM','NB'],comment=comment,header=head,extname='COR')
