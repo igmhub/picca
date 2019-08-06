9737aaddd8eaf6d9b5d34e25038ede5353b07303
londumas <helion331990@gmail.com>
2019-08-06 10:06:45 -0600

    reinstate previous code

--- py/picca/fitter2/data.py
+++ py/picca/fitter2/data.py
@@ -116,6 +116,7 @@ class data:
         w = self.r>0.
         self.mu[w] = self.rp[w]/self.r[w]
 
+
         if 'hcd_model' in dic_init:
             self.pk = pk.pk(getattr(pk, dic_init['model']['model-pk']),dic_init['hcd_model']['name_hcd_model'])
         else:
@@ -316,7 +317,7 @@ class data:
 
     def xi_model(self, k, pk_lin, pars):
         xi = self.xi(self.r, self.mu, k, pk_lin, self.pk, \
-                tracer1 = self.tracer1, tracer2 = self.tracer2, ell_max = self.ell_max, **pars)
+                    tracer1 = self.tracer1, tracer2 = self.tracer2, ell_max = self.ell_max, **pars)
 
         xi *= self.z_evol[self.tracer1['name']](self.z, self.tracer1, **pars)*self.z_evol[self.tracer2['name']](self.z, self.tracer2, **pars)
         xi *= self.growth_function(self.z, **pars)**2
