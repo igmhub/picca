42f7cf12545c27000e85270ac80f0bcba940ef6d
h.du.mas.des.bourboux@utah.edu <helion331990@gmail.com>
2020-04-13 15:15:40 +0200

    test moving bin to script

--- setup.py
+++ setup.py
@@ -1,38 +1,25 @@
 #!/usr/bin/env python
 
-#import glob
-#from setuptools import setup
-"""
-scripts = glob.glob('bin/*')
+import glob
 
-#description = "Package for Igm Cosmological-Correlations Analyses"
-#version="4.0"
-setup(
-    #name="picca",
-    #version=version,
-    #description=description,
-    #url="https://github.com/igmhub/picca",
-    #author="Nicolas Busca, Helion du Mas des Bourboux et al",
-    #author_email="nbusca@lpnhe.in2p3.fr",
+from setuptools import setup
+
+scripts = glob.glob('scripts/*')
+
+description = "Package for Igm Cosmological-Correlations Analyses"
+
+version="4.0"
+setup(name="picca",
+    version=version,
+    description=description,
+    url="https://github.com/igmhub/picca",
+    author="Nicolas Busca, Helion du Mas des Bourboux et al",
+    author_email="nbusca@lpnhe.in2p3.fr",
     packages=['picca','picca.fitter2'],
     package_dir = {'': 'py'},
     package_data = {'picca': ['fitter2/models/*/*.fits']},
-    #install_requires=['numpy','scipy','iminuit','healpy','fitsio',
-    #    'llvmlite','numba','h5py','future','setuptools'],
+    install_requires=['numpy','scipy','iminuit','healpy','fitsio',
+        'llvmlite','numba','h5py','future','setuptools'],
     test_suite='picca.test.test_cor',
     scripts = scripts
     )
-"""
-from setuptools import setup, find_packages
-
-setup(
-    name='picca',
-    version='4.0',
-    url='https://github.com/igmhub/picca',
-    author='Nicolas Busca, Helion du Mas des Bourboux et al',
-    author_email='nbusca@lpnhe.in2p3.fr',
-    description='Package for Igm Cosmological-Correlations Analyses',
-    packages=['picca'],#find_packages(),
-    install_requires=['numpy','scipy','iminuit','healpy','fitsio','llvmlite','numba','h5py','future','setuptools'],
-    #test_suite='picca.test.test_cor',
-)
