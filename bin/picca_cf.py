c8f4f7768590038787a92ff701a1342dcae24862
Ignasi Pérez Ràfols <iprafols@gmail.com>
2020-05-22 20:05:19 +0200

    added docstring, standardised parser

--- bin/picca_cf.py
+++ bin/picca_cf.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
+"""Computes Lyman alpha 3D autocorrelation
+
+Follow the procedure described in sections 3.1 and 3.2 of du Mas des Bourboux
+et al. 2020 (In prep).
+"""
 import scipy as sp
 import fitsio
 import argparse
-from multiprocessing import Pool,Lock,cpu_count,Value
+from multiprocessing import Pool, Lock, cpu_count, Value
 
 from picca import constants, cf, utils, io
 from picca.utils import userprint
@@ -15,99 +20,194 @@ def corr_func(p):
     tmp = cf.cf(p)
     return tmp
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+def main():
+    """Computes the Lyman alpha 3D autocorrelation"""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description='Compute the auto and cross-correlation of delta fields')
 
-    parser.add_argument('--out', type=str, default=None, required=True,
-        help='Output file name')
+    parser.add_argument('--out',
+                        type=str,
+                        default=None,
+                        required=True,
+                        help='Output file name')
 
-    parser.add_argument('--in-dir', type=str, default=None, required=True,
-        help='Directory to delta files')
+    parser.add_argument('--in-dir',
+                        type=str,
+                        default=None,
+                        required=True,
+                        help='Directory to delta files')
 
-    parser.add_argument('--from-image', type=str, default=None, required=False,
-        help='Read delta from image format', nargs='*')
+    parser.add_argument('--from-image',
+                        type=str,
+                        default=None,
+                        required=False,
+                        help='Read delta from image format',
+                        nargs='*')
 
-    parser.add_argument('--in-dir2', type=str, default=None, required=False,
-        help='Directory to 2nd delta files')
+    parser.add_argument('--in-dir2',
+                        type=str,
+                        default=None,
+                        required=False,
+                        help='Directory to 2nd delta files')
 
-    parser.add_argument('--rp-min', type=float, default=0., required=False,
-        help='Min r-parallel [h^-1 Mpc]')
+    parser.add_argument('--rp-min',
+                        type=float,
+                        default=0.,
+                        required=False,
+                        help='Min r-parallel [h^-1 Mpc]')
 
-    parser.add_argument('--rp-max', type=float, default=200., required=False,
-        help='Max r-parallel [h^-1 Mpc]')
+    parser.add_argument('--rp-max',
+                        type=float,
+                        default=200.,
+                        required=False,
+                        help='Max r-parallel [h^-1 Mpc]')
 
-    parser.add_argument('--rt-max', type=float, default=200., required=False,
-        help='Max r-transverse [h^-1 Mpc]')
+    parser.add_argument('--rt-max',
+                        type=float,
+                        default=200.,
+                        required=False,
+                        help='Max r-transverse [h^-1 Mpc]')
 
-    parser.add_argument('--np', type=int, default=50, required=False,
-        help='Number of r-parallel bins')
+    parser.add_argument('--np',
+                        type=int,
+                        default=50,
+                        required=False,
+                        help='Number of r-parallel bins')
 
-    parser.add_argument('--nt', type=int, default=50, required=False,
-        help='Number of r-transverse bins')
+    parser.add_argument('--nt',
+                        type=int,
+                        default=50,
+                        required=False,
+                        help='Number of r-transverse bins')
 
-    parser.add_argument('--z-cut-min', type=float, default=0., required=False,
-        help='Use only pairs of forest x object with the mean of the last absorber \
-        redshift and the object redshift larger than z-cut-min')
+    parser.add_argument('--z-cut-min',
+                        type=float,
+                        default=0.,
+                        required=False,
+                        help=('Use only pairs of forest x object with the mean '
+                              'of the last absorber redshift and the object '
+                              'redshift larger than z-cut-min'))
 
-    parser.add_argument('--z-cut-max', type=float, default=10., required=False,
-        help='Use only pairs of forest x object with the mean of the last absorber \
-        redshift and the object redshift smaller than z-cut-max')
+    parser.add_argument('--z-cut-max',
+                        type=float,
+                        default=10.,
+                        required=False,
+                        help=('Use only pairs of forest x object with the mean '
+                              'of the last absorber redshift and the object '
+                              'redshift smaller than z-cut-max'))
 
-    parser.add_argument('--lambda-abs', type=str, default='LYA', required=False,
-        help='Name of the absorption in picca.constants defining the redshift of the delta')
+    parser.add_argument('--lambda-abs',
+                        type=str,
+                        default='LYA',
+                        required=False,
+                        help=('Name of the absorption in picca.constants '
+                              'defining the redshift of the delta'))
 
-    parser.add_argument('--lambda-abs2', type=str, default=None, required=False,
-        help='Name of the absorption in picca.constants defining the redshift of the 2nd delta')
+    parser.add_argument('--lambda-abs2',
+                        type=str,
+                        default=None,
+                        required=False,
+                        help=('Name of the absorption in picca.constants '
+                              'defining the redshift of the 2nd delta'))
 
-    parser.add_argument('--z-ref', type=float, default=2.25, required=False,
-        help='Reference redshift')
+    parser.add_argument('--z-ref',
+                        type=float,
+                        default=2.25,
+                        required=False,
+                        help='Reference redshift')
 
-    parser.add_argument('--z-evol', type=float, default=2.9, required=False,
-        help='Exponent of the redshift evolution of the delta field')
+    parser.add_argument('--z-evol',
+                        type=float,
+                        default=2.9,
+                        required=False,
+                        help=('Exponent of the redshift evolution of the delta '
+                              'field'))
 
-    parser.add_argument('--z-evol2', type=float, default=2.9, required=False,
-        help='Exponent of the redshift evolution of the 2nd delta field')
+    parser.add_argument('--z-evol2',
+                        type=float,
+                        default=2.9,
+                        required=False,
+                        help=('Exponent of the redshift evolution of the 2nd '
+                              'delta field'))
 
-    parser.add_argument('--fid-Om', type=float, default=0.315, required=False,
-        help='Omega_matter(z=0) of fiducial LambdaCDM cosmology')
+    parser.add_argument('--fid-Om',
+                        type=float,
+                        default=0.315,
+                        required=False,
+                        help=('Omega_matter(z=0) of fiducial LambdaCDM '
+                              'cosmology'))
 
-    parser.add_argument('--fid-Or', type=float, default=0., required=False,
-        help='Omega_radiation(z=0) of fiducial LambdaCDM cosmology')
+    parser.add_argument('--fid-Or',
+                        type=float,
+                        default=0.,
+                        required=False,
+                        help=('Omega_radiation(z=0) of fiducial LambdaCDM '
+                              'cosmology'))
 
-    parser.add_argument('--fid-Ok', type=float, default=0., required=False,
-        help='Omega_k(z=0) of fiducial LambdaCDM cosmology')
+    parser.add_argument('--fid-Ok',
+                        type=float,
+                        default=0.,
+                        required=False,
+                        help='Omega_k(z=0) of fiducial LambdaCDM cosmology')
 
-    parser.add_argument('--fid-wl', type=float, default=-1., required=False,
-        help='Equation of state of dark energy of fiducial LambdaCDM cosmology')
+    parser.add_argument('--fid-wl',
+                        type=float,
+                        default=-1.,
+                        required=False,
+                        help=('Equation of state of dark energy of fiducial '
+                              'LambdaCDM cosmology'))
 
-    parser.add_argument('--no-project', action='store_true', required=False,
-        help='Do not project out continuum fitting modes')
+    parser.add_argument('--no-project',
+                        action='store_true',
+                        required=False,
+                        help='Do not project out continuum fitting modes')
 
-    parser.add_argument('--remove-same-half-plate-close-pairs', action='store_true', required=False,
-        help='Reject pairs in the first bin in r-parallel from same half plate')
+    parser.add_argument('--remove-same-half-plate-close-pairs',
+                        action='store_true',
+                        required=False,
+                        help=('Reject pairs in the first bin in r-parallel '
+                              'from same half plate'))
 
-    parser.add_argument('--nside', type=int, default=16, required=False,
-        help='Healpix nside')
+    parser.add_argument('--nside',
+                        type=int,
+                        default=16,
+                        required=False,
+                        help='Healpix nside')
 
-    parser.add_argument('--nproc', type=int, default=None, required=False,
-        help='Number of processors')
+    parser.add_argument('--nproc',
+                        type=int,
+                        default=None,
+                        required=False,
+                        help='Number of processors')
 
-    parser.add_argument('--nspec', type=int, default=None, required=False,
-        help='Maximum number of spectra to read')
+    parser.add_argument('--nspec',
+                        type=int,
+                        default=None,
+                        required=False,
+                        help='Maximum number of spectra to read')
 
-    parser.add_argument('--unfold-cf', action='store_true', required=False,
-        help='rp can be positive or negative depending on the relative position between absorber1 and absorber2')
+    parser.add_argument('--unfold-cf',
+                        action='store_true',
+                        required=False,
+                        help=('rp can be positive or negative depending on the '
+                              'relative position between absorber1 and '
+                              'absorber2'))
 
-    parser.add_argument('--shuffle-distrib-forest-seed', type=int, default=None, required=False,
-        help='Shuffle the distribution of forests on the sky following the given seed. Do not shuffle if None')
+    parser.add_argument('--shuffle-distrib-forest-seed',
+                        type=int,
+                        default=None,
+                        required=False,
+                        help=('Shuffle the distribution of forests on the sky '
+                              'following the given seed. Do not shuffle if '
+                              'None'))
 
     args = parser.parse_args()
 
     if args.nproc is None:
         args.nproc = cpu_count()//2
 
+    # setup variables in module cf
     cf.rp_max = args.rp_max
     cf.rt_max = args.rt_max
     cf.rp_min = args.rp_min
@@ -212,3 +312,6 @@ if __name__ == '__main__':
         header=head2,extname='COR')
 
     out.close()
+
+if __name__ == '__main__':
+    main()
