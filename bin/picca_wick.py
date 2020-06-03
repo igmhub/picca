384d3fdbbf345c0e8ce6f07c2ef8e86e5bae3e4b
Ignasi Pérez Ràfols <iprafols@gmail.com>
2020-06-03 11:04:24 +0200

    standardized module bin.picca_wick

--- bin/picca_wick.py
+++ bin/picca_wick.py
@@ -4,23 +4,36 @@
 The wick covariance is computed as explained in Delubac et al. 2015
 """
 import sys
-import fitsio
 import argparse
+from multiprocessing import Pool, Lock, cpu_count, Value
+import fitsio
 import numpy as np
-import scipy as sp
 from scipy.interpolate import interp1d
-from multiprocessing import Pool,Lock,cpu_count,Value
 
 from picca import constants, cf, utils, io
 from picca.utils import userprint
 
-def calc_wickT(p):
-    cf.fill_neighs(p)
-    np.random.seed(p[0])
-    tmp = cf.compute_wick_terms(p)
-    return tmp
+def calc_wick_terms(healpixs):
+    """Computes the wick expansion terms of the covariance matrix.
+
+    To optimize the computation, first compute a list of neighbours for each of
+    the healpix. This is an auxiliar function to split the computational load
+    using several CPUs.
+
+    Args:
+        healpixs: array of ints
+            List of healpix numbers
+
+    Returns:
+        The correlation function data
+    """
+    cf.fill_neighs(healpixs)
+    np.random.seed(healpixs[0])
+    wick_data = cf.compute_wick_terms(healpixs)
+    return wick_data
 
 def main():
+    # pylint: disable-msg=too-many-locals,too-many-branches,too-many-statements
     """Computes the wick covariance for the auto-correlation of forests"""
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -276,66 +289,86 @@ def main():
     cf.reject = args.rej
     cf.max_diagram = args.max_diagram
 
-    ### Cosmo
+    # load cosmology
     if (args.fid_Or != 0.) or (args.fid_Ok != 0.) or (args.fid_wl != -1.):
-        userprint("ERROR: Cosmology with other than Omega_m set are not yet implemented")
+        userprint(("ERROR: Cosmology with other than Omega_m set are not yet "
+                   "implemented"))
         sys.exit()
-    cosmo = constants.Cosmo(Om=args.fid_Om,Or=args.fid_Or,Ok=args.fid_Ok,wl=args.fid_wl)
+    cosmo = constants.Cosmo(Om=args.fid_Om,
+                            Or=args.fid_Or,
+                            Ok=args.fid_Ok,
+                            wl=args.fid_wl)
 
-    ### Read data
-    data, num_data, zmin_pix, zmax_pix = io.read_deltas(args.in_dir, cf.nside, cf.lambda_abs, cf.alpha, cf.z_ref, cosmo, max_num_spec=args.nspec)
-    for p,datap in data.items():
-        for d in datap:
-            d.fname = 'D1'
-            for k in ['cont','delta','order','ivar','exposures_diff','mean_snr','mean_reso','mean_z','delta_log_lambda']:
-                setattr(d,k,None)
-    cf.npix = len(data)
+    # read data 1
+    data, num_data, z_min, z_max = io.read_deltas(args.in_dir,
+                                                  cf.nside,
+                                                  cf.lambda_abs,
+                                                  cf.alpha,
+                                                  cf.z_ref,
+                                                  cosmo,
+                                                  max_num_spec=args.nspec)
+    for deltas in data.values():
+        for delta in deltas:
+            delta.fname = 'D1'
+            for item in ['cont', 'delta', 'order', 'ivar', 'exposures_diff',
+                         'mean_snr', 'mean_reso', 'mean_z', 'delta_log_lambda']:
+                setattr(delta, item, None)
+    del z_max
     cf.data = data
     cf.num_data = num_data
-    cf.ang_max = utils.compute_ang_max(cosmo,cf.r_trans_max,zmin_pix)
+    cf.ang_max = utils.compute_ang_max(cosmo, cf.r_trans_max, z_min)
     sys.stderr.write("\n")
-    userprint("done, npix = {}".format(cf.npix))
+    userprint("done, npix = {}".format(len(data)))
 
-    ### Load cf1d
-    dic_cf1d = { 'D1':args.cf1d, 'D2':args.cf1d2 }
-    for n,p in dic_cf1d.items():
-        if p is None:
+    ### Load 1d correlation functions
+    dic_xi1d = {"D1": args.cf1d, "D2": args.cf1d2}
+    for fname, filename in dic_xi1d.items():
+        if filename is None:
             continue
-        h = fitsio.FITS(p)
-        head = h[1].read_header()
-        log_lambda_min = head['LLMIN']
-        log_lambda_max = head['LLMAX']
-        delta_log_lambda = head['DLL']
-        num_pairs_variance_1d = h[1]['nv1d'][:]
-        variance_1d = h[1]['v1d'][:]
-        log_lambda = log_lambda_min + delta_log_lambda*np.arange(len(variance_1d))
-        cf.get_variance_1d[n] = interp1d(log_lambda[num_pairs_variance_1d>0],variance_1d[num_pairs_variance_1d>0],kind='nearest',fill_value='extrapolate')
+        hdul = fitsio.FITS(filename)
+        header = hdul[1].read_header()
+        log_lambda_min = header['LLMIN']
+        delta_log_lambda = header['DLL']
+        num_pairs_variance_1d = hdul[1]['nv1d'][:]
+        variance_1d = hdul[1]['v1d'][:]
+        log_lambda = (log_lambda_min +
+                      delta_log_lambda*np.arange(len(variance_1d)))
+        cf.get_variance_1d[fname] = interp1d(log_lambda[num_pairs_variance_1d > 0],
+                                             variance_1d[num_pairs_variance_1d > 0],
+                                             kind='nearest',
+                                             fill_value='extrapolate')
 
-        num_pairs1d = h[1]['nb1d'][:]
-        xi_1d = h[1]['c1d'][:]
-        cf.xi_1d[n] = interp1d((log_lambda-log_lambda_min)[num_pairs1d>0],xi_1d[num_pairs1d>0],kind='nearest',fill_value='extrapolate')
-        h.close()
+        num_pairs1d = hdul[1]['nb1d'][:]
+        xi_1d = hdul[1]['c1d'][:]
+        cf.xi_1d[fname] = interp1d((log_lambda - log_lambda_min)[num_pairs1d > 0],
+                                   xi_1d[num_pairs1d > 0],
+                                   kind='nearest',
+                                   fill_value='extrapolate')
+        hdul.close()
 
-    ### Load cf
-    dic_cf = { 'D1_D1':args.cf, 'D2_D2':args.cf2, 'D1_D2':args.cf12, 'D2_D1':args.cf12 }
-    for n,p in dic_cf.items():
-        if p is None:
+    # Load correlation functions
+    dic_xi = {"D1_D1": args.cf,
+              "D2_D2": args.cf2,
+              "D1_D2": args.cf12,
+              "D2_D1": args.cf12}
+    for fname, filename in dic_xi.items():
+        if filename is None:
             continue
-        h = fitsio.FITS(p)
-        head = h[1].read_header()
-        assert cf.num_bins_r_par == head['NP']
-        assert cf.num_bins_r_trans == head['NT']
-        assert cf.r_par_min == head['RPMIN']
-        assert cf.r_par_max == head['RPMAX']
-        assert cf.r_trans_max == head['RTMAX']
-        da = h[2]['DA'][:]
-        weights = h[2]['WE'][:]
+        hdul = fitsio.FITS(filename)
+        header = hdul[1].read_header()
+        assert cf.num_bins_r_par == header['NP']
+        assert cf.num_bins_r_trans == header['NT']
+        assert cf.r_par_min == header['RPMIN']
+        assert cf.r_par_max == header['RPMAX']
+        assert cf.r_trans_max == header['RTMAX']
+        da = hdul[2]['DA'][:]
+        weights = hdul[2]['WE'][:]
         da = (da*weights).sum(axis=0)
         weights = weights.sum(axis=0)
-        w = weights>0.
+        w = weights > 0.
         da[w] /= weights[w]
-        cf.xi_wick[n] = da.copy()
-        h.close()
+        cf.xi_wick[fname] = da.copy()
+        hdul.close()
 
     ### Read data 2
     if args.in_dir2 or args.lambda_abs2:
@@ -350,77 +383,102 @@ def main():
         else:
             cf.lambda_abs2 = cf.lambda_abs
 
-        data2, num_data2, zmin_pix2, zmax_pix2 = io.read_deltas(args.in_dir2, cf.nside, cf.lambda_abs2, cf.alpha2, cf.z_ref, cosmo, max_num_spec=args.nspec)
-        for p,datap in data2.items():
-            for d in datap:
-                d.fname = 'D2'
-                for k in ['cont','delta','order','ivar','exposures_diff','mean_snr','mean_reso','mean_z','delta_log_lambda']:
-                    setattr(d,k,None)
+        data2, num_data2, z_min2, z_max2 = io.read_deltas(args.in_dir2,
+                                                          cf.nside,
+                                                          cf.lambda_abs2,
+                                                          cf.alpha2,
+                                                          cf.z_ref,
+                                                          cosmo,
+                                                          max_num_spec=args.nspec)
+        for deltas in data.values():
+            for delta in deltas:
+                delta.fname = 'D2'
+                for item in ['cont', 'delta', 'order', 'ivar', 'exposures_diff',
+                             'mean_snr', 'mean_reso', 'mean_z',
+                             'delta_log_lambda']:
+                    setattr(delta, item, None)
+        del z_max2
         cf.data2 = data2
         cf.num_data2 = num_data2
-        cf.ang_max = utils.compute_ang_max(cosmo,cf.r_trans_max,zmin_pix,zmin_pix2)
+        cf.ang_max = utils.compute_ang_max(cosmo, cf.r_trans_max, z_min, z_min2)
         userprint("")
         userprint("done, npix = {}".format(len(data2)))
 
-    cf.counter = Value('i',0)
+    cf.counter = Value('i', 0)
     cf.lock = Lock()
 
     cpu_data = {}
-    for i,p in enumerate(sorted(data.keys())):
-        ip = i%args.nproc
-        if not ip in cpu_data:
-            cpu_data[ip] = []
-        cpu_data[ip].append(p)
+    for index, healpix in enumerate(sorted(data)):
+        num_processor = index % args.nproc
+        if not num_processor in cpu_data:
+            cpu_data[num_processor] = []
+        cpu_data[num_processor].append(healpix)
 
-    pool = Pool(processes=min(args.nproc,len(cpu_data.values())))
+    # compute the covariance matrix
+    pool = Pool(processes=min(args.nproc, len(cpu_data.values())))
     userprint(" \nStarting\n")
-    wickT = pool.map(calc_wickT,sorted(cpu_data.values()))
+    wick_data = pool.map(calc_wick_terms, sorted(cpu_data.values()))
     userprint(" \nFinished\n")
     pool.close()
 
-    wickT = sp.array(wickT)
-    weights_wick = wickT[:,0].sum(axis=0)
-    num_pairs_wick = wickT[:,1].sum(axis=0)
-    npairs = wickT[:,2].sum(axis=0)
-    npairs_used = wickT[:,3].sum(axis=0)
-    t1 = wickT[:,4].sum(axis=0)
-    t2 = wickT[:,5].sum(axis=0)
-    t3 = wickT[:,6].sum(axis=0)
-    t4 = wickT[:,7].sum(axis=0)
-    t5 = wickT[:,8].sum(axis=0)
-    t6 = wickT[:,9].sum(axis=0)
-    weights = weights_wick*weights_wick[:,None]
-    w = weights>0.
+    wick_data = np.array(wick_data)
+    weights_wick = wick_data[:, 0].sum(axis=0)
+    num_pairs_wick = wick_data[:, 1].sum(axis=0)
+    num_pairs = wick_data[:, 2].sum(axis=0)
+    num_pairs_used = wick_data[:, 3].sum(axis=0)
+    t1 = wick_data[:, 4].sum(axis=0)
+    t2 = wick_data[:, 5].sum(axis=0)
+    t3 = wick_data[:, 6].sum(axis=0)
+    t4 = wick_data[:, 7].sum(axis=0)
+    t5 = wick_data[:, 8].sum(axis=0)
+    t6 = wick_data[:, 9].sum(axis=0)
+    weights = weights_wick*weights_wick[:, None]
+    w = weights > 0.
     t1[w] /= weights[w]
     t2[w] /= weights[w]
     t3[w] /= weights[w]
     t4[w] /= weights[w]
     t5[w] /= weights[w]
     t6[w] /= weights[w]
-    t1 *= 1.*npairs_used/npairs
-    t2 *= 1.*npairs_used/npairs
-    t3 *= 1.*npairs_used/npairs
-    t4 *= 1.*npairs_used/npairs
-    t5 *= 1.*npairs_used/npairs
-    t6 *= 1.*npairs_used/npairs
-    Ttot = t1+t2+t3+t4+t5+t6
+    t1 *= 1.*num_pairs_used/num_pairs
+    t2 *= 1.*num_pairs_used/num_pairs
+    t3 *= 1.*num_pairs_used/num_pairs
+    t4 *= 1.*num_pairs_used/num_pairs
+    t5 *= 1.*num_pairs_used/num_pairs
+    t6 *= 1.*num_pairs_used/num_pairs
+    t_tot = t1 + t2 + t3 + t4 + t5 + t6
 
-    out = fitsio.FITS(args.out,'rw',clobber=True)
-    head = [
-        {'name':'RPMIN','value':cf.r_par_min,'comment':'Minimum r-parallel [h^-1 Mpc]'},
-        {'name':'RPMAX','value':cf.r_par_max,'comment':'Maximum r-parallel [h^-1 Mpc]'},
-        {'name':'RTMAX','value':cf.r_trans_max,'comment':'Maximum r-transverse [h^-1 Mpc]'},
-        {'name':'NP','value':cf.num_bins_r_par,'comment':'Number of bins in r-parallel'},
-        {'name':'NT','value':cf.num_bins_r_trans,'comment':'Number of bins in r-transverse'},
-        {'name':'ZCUTMIN','value':cf.z_cut_min,'comment':'Minimum redshift of pairs'},
-        {'name':'ZCUTMAX','value':cf.z_cut_max,'comment':'Maximum redshift of pairs'},
-        {'name':'REJ','value':cf.reject,'comment':'Rejection factor'},
-        {'name':'NPALL','value':npairs,'comment':'Number of pairs'},
-        {'name':'NPUSED','value':npairs_used,'comment':'Number of used pairs'},
+    results = fitsio.FITS(args.out, 'rw', clobber=True)
+    header = [
+        {'name': 'RPMIN', 'value': cf.r_par_min,
+         'comment': 'Minimum r-parallel [h^-1 Mpc]'},
+        {'name': 'RPMAX', 'value': cf.r_par_max,
+         'comment': 'Maximum r-parallel [h^-1 Mpc]'},
+        {'name': 'RTMAX', 'value': cf.r_trans_max,
+         'comment': 'Maximum r-transverse [h^-1 Mpc]'},
+        {'name': 'NP', 'value': cf.num_bins_r_par,
+         'comment': 'Number of bins in r-parallel'},
+        {'name': 'NT', 'value': cf.num_bins_r_trans,
+         'comment': 'Number of bins in r-transverse'},
+        {'name': 'ZCUTMIN', 'value': cf.z_cut_min,
+         'comment':'Minimum redshift of pairs'},
+        {'name': 'ZCUTMAX', 'value': cf.z_cut_max,
+         'comment': 'Maximum redshift of pairs'},
+        {'name': 'REJ', 'value': cf.reject,
+         'comment': 'Rejection factor'},
+        {'name': 'NPALL', 'value': num_pairs,
+         'comment': 'Number of pairs'},
+        {'name': 'NPUSED', 'value': num_pairs_used,
+         'comment': 'Number of used pairs'},
     ]
-    comment = ['Sum of weight','Covariance','Nomber of pairs','T1','T2','T3','T4','T5','T6']
-    out.write([Ttot,weights_wick,num_pairs_wick,t1,t2,t3,t4,t5,t6],names=['CO','WALL','NB','T1','T2','T3','T4','T5','T6'],comment=comment,header=head,extname='COV')
-    out.close()
+    comment = ['Sum of weight', 'Covariance', 'Nomber of pairs', 'T1', 'T2',
+               'T3', 'T4', 'T5', 'T6']
+    results.write([t_tot, weights_wick, num_pairs_wick, t1, t2, t3, t4, t5, t6],
+                  names=['CO', 'WALL', 'NB', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6'],
+                  comment=comment,
+                  header=header,
+                  extname='COV')
+    results.close()
 
 
 if __name__ == '__main__':
