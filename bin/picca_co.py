fa6fd8591373537e24a6cf545c4c0f1a4a92a535
Ignasi Pérez Ràfols <iprafols@gmail.com>
2020-06-08 12:57:21 +0200

    applied yapf formatter

--- bin/picca_co.py
+++ bin/picca_co.py
@@ -10,6 +10,7 @@ import fitsio
 from picca import constants, co, io, utils
 from picca.utils import userprint
 
+
 def corr_func(healpixs):
     """Computes the correlation function.
 
@@ -28,6 +29,7 @@ def corr_func(healpixs):
     correlation_function_data = co.compute_xi(healpixs)
     return correlation_function_data
 
+
 def main():
     """Compute the auto and cross-correlation between catalogs of objects"""
 
@@ -36,61 +38,53 @@ def main():
         description=('Compute the auto and cross-correlation between catalogs '
                      'of objects'))
 
-    parser.add_argument(
-        '--out',
-        type=str,
-        default=None,
-        required=True,
-        help='Output file name')
+    parser.add_argument('--out',
+                        type=str,
+                        default=None,
+                        required=True,
+                        help='Output file name')
 
-    parser.add_argument(
-        '--drq',
-        type=str,
-        default=None,
-        required=True,
-        help='Catalog of objects in DRQ format')
+    parser.add_argument('--drq',
+                        type=str,
+                        default=None,
+                        required=True,
+                        help='Catalog of objects in DRQ format')
 
-    parser.add_argument(
-        '--drq2',
-        type=str,
-        default=None,
-        required=False,
-        help='Catalog of objects 2 in DRQ format')
+    parser.add_argument('--drq2',
+                        type=str,
+                        default=None,
+                        required=False,
+                        help='Catalog of objects 2 in DRQ format')
 
-    parser.add_argument(
-        '--rp-min',
-        type=float,
-        default=0.,
-        required=False,
-        help='Min r-parallel [h^-1 Mpc]')
+    parser.add_argument('--rp-min',
+                        type=float,
+                        default=0.,
+                        required=False,
+                        help='Min r-parallel [h^-1 Mpc]')
 
-    parser.add_argument(
-        '--rp-max',
-        type=float,
-        default=200.,
-        required=False,
-        help='Max r-parallel [h^-1 Mpc]')
+    parser.add_argument('--rp-max',
+                        type=float,
+                        default=200.,
+                        required=False,
+                        help='Max r-parallel [h^-1 Mpc]')
 
-    parser.add_argument(
-        '--rt-max',
-        type=float,
-        default=200.,
-        required=False,
-        help='Max r-transverse [h^-1 Mpc]')
+    parser.add_argument('--rt-max',
+                        type=float,
+                        default=200.,
+                        required=False,
+                        help='Max r-transverse [h^-1 Mpc]')
 
-    parser.add_argument(
-        '--np',
-        type=int,
-        default=50,
-        required=False,
-        help='Number of r-parallel bins')
+    parser.add_argument('--np',
+                        type=int,
+                        default=50,
+                        required=False,
+                        help='Number of r-parallel bins')
 
-    parser.add_argument(
-        '--nt',
-        type=int,
-        default=50,
-        required=False,
-        help='Number of r-transverse bins')
+    parser.add_argument('--nt',
+                        type=int,
+                        default=50,
+                        required=False,
+                        help='Number of r-transverse bins')
 
     parser.add_argument(
         '--z-cut-min',
@@ -108,26 +102,23 @@ def main():
         help=('Use only pairs of object x object with the mean redshift lower '
               'than z-cut-max'))
 
-    parser.add_argument(
-        '--z-min-obj',
-        type=float,
-        default=None,
-        required=False,
-        help='Min redshift for object field')
+    parser.add_argument('--z-min-obj',
+                        type=float,
+                        default=None,
+                        required=False,
+                        help='Min redshift for object field')
 
-    parser.add_argument(
-        '--z-max-obj',
-        type=float,
-        default=None,
-        required=False,
-        help='Max redshift for object field')
+    parser.add_argument('--z-max-obj',
+                        type=float,
+                        default=None,
+                        required=False,
+                        help='Max redshift for object field')
 
-    parser.add_argument(
-        '--z-ref',
-        type=float,
-        default=2.25,
-        required=False,
-        help='Reference redshift')
+    parser.add_argument('--z-ref',
+                        type=float,
+                        default=2.25,
+                        required=False,
+                        help='Reference redshift')
 
     parser.add_argument(
         '--z-evol-obj',
@@ -157,12 +148,11 @@ def main():
         required=False,
         help='Omega_radiation(z=0) of fiducial LambdaCDM cosmology')
 
-    parser.add_argument(
-        '--fid-Ok',
-        type=float,
-        default=0.,
-        required=False,
-        help='Omega_k(z=0) of fiducial LambdaCDM cosmology')
+    parser.add_argument('--fid-Ok',
+                        type=float,
+                        default=0.,
+                        required=False,
+                        help='Omega_k(z=0) of fiducial LambdaCDM cosmology')
 
     parser.add_argument(
         '--fid-wl',
@@ -178,24 +168,22 @@ def main():
         required=False,
         help='type of correlation: DD, RR, DR, RD, xDD, xRR, xD1R2, xR1D2')
 
-    parser.add_argument(
-        '--nside',
-        type=int,
-        default=16,
-        required=False,
-        help='Healpix nside')
+    parser.add_argument('--nside',
+                        type=int,
+                        default=16,
+                        required=False,
+                        help='Healpix nside')
 
-    parser.add_argument(
-        '--nproc',
-        type=int,
-        default=None,
-        required=False,
-        help='Number of processors')
+    parser.add_argument('--nproc',
+                        type=int,
+                        default=None,
+                        required=False,
+                        help='Number of processors')
 
     args = parser.parse_args()
 
     if args.nproc is None:
-        args.nproc = cpu_count()//2
+        args.nproc = cpu_count() // 2
 
     # setup variables in module co
     co.r_par_max = args.rp_max
@@ -207,8 +195,9 @@ def main():
     co.num_bins_r_trans = args.nt
     co.nside = args.nside
     co.type_corr = args.type_corr
-    if co.type_corr not in ['DD', 'RR', 'DR', 'RD', 'xDD', 'xRR', 'xD1R2',
-                            'xR1D2']:
+    if co.type_corr not in [
+            'DD', 'RR', 'DR', 'RD', 'xDD', 'xRR', 'xD1R2', 'xR1D2'
+    ]:
         userprint(("ERROR: type-corr not in ['DD', 'RR', 'DR', 'RD', 'xDD', "
                    "'xRR', 'xD1R2', 'xR1D2']"))
         sys.exit()
@@ -224,12 +213,8 @@ def main():
                             wl=args.fid_wl)
 
     ### Read objects 1
-    objs, z_min = io.read_objects(args.drq,
-                                  args.nside,
-                                  args.z_min_obj,
-                                  args.z_max_obj,
-                                  args.z_evol_obj,
-                                  args.z_ref,
+    objs, z_min = io.read_objects(args.drq, args.nside, args.z_min_obj,
+                                  args.z_max_obj, args.z_evol_obj, args.z_ref,
                                   cosmo)
     userprint("")
     co.objs = objs
@@ -239,13 +224,9 @@ def main():
 
     ### Read objects 2
     if co.x_correlation:
-        objs2, z_min2 = io.read_objects(args.drq2,
-                                        args.nside,
-                                        args.z_min_obj,
-                                        args.z_max_obj,
-                                        args.z_evol_obj2,
-                                        args.z_ref,
-                                        cosmo)
+        objs2, z_min2 = io.read_objects(args.drq2, args.nside, args.z_min_obj,
+                                        args.z_max_obj, args.z_evol_obj2,
+                                        args.z_ref, cosmo)
         userprint("")
         co.objs2 = objs2
         # recompute maximum angular separation
@@ -270,63 +251,96 @@ def main():
     healpix_list = np.array(sorted(list(cpu_data)))
 
     w = (weights_list.sum(axis=0) > 0.)
-    r_par = (r_par_list*weights_list).sum(axis=0)
+    r_par = (r_par_list * weights_list).sum(axis=0)
     r_par[w] /= weights_list.sum(axis=0)[w]
-    r_trans = (r_trans_list*weights_list).sum(axis=0)
+    r_trans = (r_trans_list * weights_list).sum(axis=0)
     r_trans[w] /= weights_list.sum(axis=0)[w]
-    z = (z_list*weights_list).sum(axis=0)
+    z = (z_list * weights_list).sum(axis=0)
     z[w] /= weights_list.sum(axis=0)[w]
     num_pairs = num_pairs_list.sum(axis=0)
 
-
     results = fitsio.FITS(args.out, 'rw', clobber=True)
     header = [
-        {'name': 'RPMIN', 'value': co.r_par_min,
-         'comment': 'Minimum r-parallel [h^-1 Mpc]'},
-        {'name': 'RPMAX', 'value': co.r_par_max,
-         'comment': 'Maximum r-parallel [h^-1 Mpc]'},
-        {'name': 'RTMAX', 'value': co.r_trans_max,
-         'comment': 'Maximum r-transverse [h^-1 Mpc]'},
-        {'name': 'NP', 'value': co.num_bins_r_par,
-         'comment': 'Number of bins in r-parallel'},
-        {'name': 'NT', 'value': co.num_bins_r_trans,
-         'comment': 'Number of bins in r-transverse'},
-        {'name': 'NSIDE', 'value': co.nside,
-         'comment': 'Healpix nside'},
-        {'name': 'TYPECORR', 'value': co.type_corr,
-         'comment':'Correlation type'},
-        {'name': 'NOBJ', 'value': len([obj for healpix in co.objs
-                                       for obj in co.objs[healpix]]),
-         'comment': 'Number of objects'},
+        {
+            'name': 'RPMIN',
+            'value': co.r_par_min,
+            'comment': 'Minimum r-parallel [h^-1 Mpc]'
+        },
+        {
+            'name': 'RPMAX',
+            'value': co.r_par_max,
+            'comment': 'Maximum r-parallel [h^-1 Mpc]'
+        },
+        {
+            'name': 'RTMAX',
+            'value': co.r_trans_max,
+            'comment': 'Maximum r-transverse [h^-1 Mpc]'
+        },
+        {
+            'name': 'NP',
+            'value': co.num_bins_r_par,
+            'comment': 'Number of bins in r-parallel'
+        },
+        {
+            'name': 'NT',
+            'value': co.num_bins_r_trans,
+            'comment': 'Number of bins in r-transverse'
+        },
+        {
+            'name': 'NSIDE',
+            'value': co.nside,
+            'comment': 'Healpix nside'
+        },
+        {
+            'name': 'TYPECORR',
+            'value': co.type_corr,
+            'comment': 'Correlation type'
+        },
+        {
+            'name':
+                'NOBJ',
+            'value':
+                len([obj for healpix in co.objs for obj in co.objs[healpix]]),
+            'comment':
+                'Number of objects'
+        },
     ]
     if co.x_correlation:
-        header += [{'name': 'NOBJ2',
-                    'value': len([obj2 for healpix in co.objs2
-                                  for obj2 in co.objs2[healpix]]),
-                    'comment': 'Number of objects 2'}]
+        header += [{
+            'name':
+                'NOBJ2',
+            'value':
+                len([
+                    obj2 for healpix in co.objs2 for obj2 in co.objs2[healpix]
+                ]),
+            'comment':
+                'Number of objects 2'
+        }]
 
     comment = ['R-parallel', 'R-transverse', 'Redshift', 'Number of pairs']
     units = ['h^-1 Mpc', 'h^-1 Mpc', '', '']
-    results.write(
-        [r_par, r_trans, z, num_pairs],
-        names=['RP', 'RT', 'Z', 'NB'],
-        headerer=header,
-        comment=comment,
-        units=units,
-        extname='ATTRI')
+    results.write([r_par, r_trans, z, num_pairs],
+                  names=['RP', 'RT', 'Z', 'NB'],
+                  headerer=header,
+                  comment=comment,
+                  units=units,
+                  extname='ATTRI')
 
     comment = ['Healpix index', 'Sum of weight', 'Number of pairs']
-    header2 = [{'name': 'HLPXSCHM', 'value': 'RING',
-                'comment': 'healpix scheme'}]
-    results.write(
-        [healpix_list, weights_list, num_pairs_list],
-        names=['HEALPID', 'WE', 'NB'],
-        header=header2,
-        comment=comment,
-        extname='COR')
+    header2 = [{
+        'name': 'HLPXSCHM',
+        'value': 'RING',
+        'comment': 'healpix scheme'
+    }]
+    results.write([healpix_list, weights_list, num_pairs_list],
+                  names=['HEALPID', 'WE', 'NB'],
+                  header=header2,
+                  comment=comment,
+                  extname='COR')
     results.close()
 
     userprint("\nFinished")
+
 
 if __name__ == '__main__':
     main()
