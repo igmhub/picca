dc855e9b02a04975023d92d8fec26413aaff50e2
Ignasi Pérez Ràfols <iprafols@gmail.com>
2020-06-08 11:58:24 +0200

    standardized module picca.co

--- py/picca/co.py
+++ py/picca/co.py
@@ -8,7 +8,6 @@ This module provides several functions:
 See the respective docstrings for more details
 """
 import numpy as np
-import scipy as sp
 from healpy import query_disc
 from numba import jit
 
@@ -71,67 +70,132 @@ def fill_neighs(healpixs):
                                             (obj2.z_qso + obj1.z_qso)/2. < z_cut_max)])
 
 
-def co(healpixs):
+def compute_xi(healpixs):
+    """Computes the correlation function for each of the healpixs.
 
+    Args:
+        healpixs: array of ints
+            List of healpix numbers
+
+    Returns:
+        The following variables:
+            weights: Total weights in the correlation function pixels
+            r_par: Parallel distance of the correlation function pixels
+            r_trans: Transverse distance of the correlation function pixels
+            z: Redshift of the correlation function pixels
+            num_pairs: Number of pairs in the correlation function pixels
+    """
     weights = np.zeros(num_bins_r_par*num_bins_r_trans)
     r_par = np.zeros(num_bins_r_par*num_bins_r_trans)
     r_trans = np.zeros(num_bins_r_par*num_bins_r_trans)
-    z  = np.zeros(num_bins_r_par*num_bins_r_trans)
-    nb = np.zeros(num_bins_r_par*num_bins_r_trans,dtype=sp.int64)
+    z = np.zeros(num_bins_r_par*num_bins_r_trans)
+    num_pairs = np.zeros(num_bins_r_par*num_bins_r_trans, dtype=np.int64)
 
     for healpix in healpixs:
         for obj1 in objs[healpix]:
 
-            userprint("\rcomputing xi: {}%".format(round(counter.value*100./num_data,2)),end="")
+            userprint(("\rcomputing xi: "
+                       "{}%").format(round(counter.value*100./num_data, 2)),
+                      end="")
             with lock:
                 counter.value += 1
 
-            if (obj1.neighbours.size == 0): continue
+            if obj1.neighbours.size == 0:
+                continue
 
-            ang      = obj1^obj1.neighbours
-            zo2      = sp.array([obj2.z_qso    for obj2 in obj1.neighbours])
-            r_comov2 = sp.array([obj2.r_comov for obj2 in obj1.neighbours])
-            dist_m2 = sp.array([obj2.dist_m for obj2 in obj1.neighbours])
-            weo2     = sp.array([obj2.weights      for obj2 in obj1.neighbours])
+            ang = obj1^obj1.neighbours
+            z2 = np.array([obj2.z_qso for obj2 in obj1.neighbours])
+            r_comov2 = np.array([obj2.r_comov for obj2 in obj1.neighbours])
+            dist_m2 = np.array([obj2.dist_m for obj2 in obj1.neighbours])
+            weights2 = np.array([obj2.weights for obj2 in obj1.neighbours])
 
-            cw,crp,crt,cz,cnb = fast_co(obj1.z_qso,obj1.r_comov,obj1.dist_m,obj1.weights,zo2,r_comov2,dist_m2,weo2,ang)
+            (rebin_weight, rebin_r_par, rebin_r_trans, rebin_z,
+             rebin_num_pairs) = compute_xi_forest_pairs(obj1.z_qso,
+                                                        obj1.r_comov,
+                                                        obj1.dist_m,
+                                                        obj1.weights,
+                                                        z2,
+                                                        r_comov2,
+                                                        dist_m2,
+                                                        weights2,
+                                                        ang)
 
-            weights[:len(cw)]  += cw
-            r_par[:len(crp)] += crp
-            r_trans[:len(crp)] += crt
-            z[:len(crp)]  += cz
-            nb[:len(cnb)] += cnb
-            setattr(obj1,"neighbours",None)
+            weights[:len(rebin_weight)] += rebin_weight
+            r_par[:len(rebin_r_par)] += rebin_r_par
+            r_trans[:len(rebin_r_trans)] += rebin_r_trans
+            z[:len(rebin_z)] += rebin_z
+            num_pairs[:len(rebin_num_pairs)] += rebin_num_pairs
+            setattr(obj1, "neighbours", None)
 
-    w = weights>0.
+    w = weights > 0.
     r_par[w] /= weights[w]
     r_trans[w] /= weights[w]
-    z[w]  /= weights[w]
-    return weights,r_par,r_trans,z,nb
+    z[w] /= weights[w]
+    return weights, r_par, r_trans, z, num_pairs
+
+
 @jit
-def fast_co(z1,r1,rdm1,w1,z2,r2,rdm2,w2,ang):
+def compute_xi_forest_pairs(z1, r_comov1, dist_m1, weights1, z2, r_comov2,
+                            dist_m2, weights2, ang):
+    """Computes the contribution of a given pair of forests to the correlation
+    function.
 
-    r_par  = (r1-r2)*sp.cos(ang/2.)
-    if not x_correlation or type_corr in ['DR','RD']:
+    Args:
+        z1: array of float
+            Redshift of pixel 1
+        r_comov1: array of float
+            Comoving distance for forest 1 (in Mpc/h)
+        dist_m1: array of float
+            Comoving angular distance for forest 1 (in Mpc/h)
+        weights1: array of float
+            Pixel weights for forest 1
+        z2: array of float
+            Redshift of pixel 2
+        r_comov2: array of float
+            Comoving distance for forest 2 (in Mpc/h)
+        dist_m2: array of float
+            Comoving angular distance for forest 2 (in Mpc/h)
+        weights2: array of float
+            Pixel weights for forest 2
+        ang: array of float
+            Angular separation between pixels in forests 1 and 2
+
+    Returns:
+        The following variables:
+            rebin_weight: The weight of the correlation function pixels
+                properly rebinned
+            rebin_r_par: The parallel distance of the correlation function
+                pixels properly rebinned
+            rebin_r_trans: The transverse distance of the correlation function
+                pixels properly rebinned
+            rebin_z: The redshift of the correlation function pixels properly
+                rebinned
+            rebin_num_pairs: The number of pairs of the correlation function
+                pixels properly rebinned
+    """
+    r_par = (r_comov1 - r_comov2)*np.cos(ang/2.)
+    if not x_correlation or type_corr in ['DR', 'RD']:
         r_par = np.absolute(r_par)
-    r_trans  = (rdm1+rdm2)*sp.sin(ang/2.)
-    z   = (z1+z2)/2.
-    w12 = w1*w2
+    r_trans = (dist_m1 + dist_m2)*np.sin(ang/2.)
+    z = (z1 + z2)/2.
+    weights12 = weights1*weights2
 
-    w   = (r_par>=r_par_min) & (r_par<r_par_max) & (r_trans<r_trans_max) & (w12>0.)
-    r_par  = r_par[w]
-    r_trans  = r_trans[w]
-    z   = z[w]
-    w12 = w12[w]
+    w = (r_par >= r_par_min) & (r_par < r_par_max) & (r_trans < r_trans_max)
+    w &= (weights12 > 0.)
+    r_par = r_par[w]
+    r_trans = r_trans[w]
+    z = z[w]
+    weights12 = weights12[w]
 
-    bp   = sp.floor((r_par-r_par_min)/(r_par_max-r_par_min)*num_bins_r_par).astype(int)
-    bt   = (r_trans/r_trans_max*num_bins_r_trans).astype(int)
-    bins = bt + num_bins_r_trans*bp
+    bins_r_par = np.floor((r_par - r_par_min)/(r_par_max - r_par_min)*
+                          num_bins_r_par).astype(int)
+    bins_r_trans = (r_trans/r_trans_max*num_bins_r_trans).astype(int)
+    bins = bins_r_trans + num_bins_r_trans*bins_r_par
 
-    cw  = sp.bincount(bins,weights=w12)
-    crp = sp.bincount(bins,weights=r_par*w12)
-    crt = sp.bincount(bins,weights=r_trans*w12)
-    cz  = sp.bincount(bins,weights=z*w12)
-    cnb = sp.bincount(bins)
+    rebin_weight = np.bincount(bins, weights=weights12)
+    rebin_r_par = np.bincount(bins, weights=r_par*weights12)
+    rebin_r_trans = np.bincount(bins, weights=r_trans*weights12)
+    rebin_z = np.bincount(bins, weights=z*weights12)
+    rebin_num_pairs = np.bincount(bins)
 
-    return cw,crp,crt,cz,cnb
+    return rebin_weight, rebin_r_par, rebin_r_trans, rebin_z, rebin_num_pairs
