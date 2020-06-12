d690ddb6da0e7fda0b05db4fc53148bdeeb68a04
Ignasi Pérez Ràfols <iprafols@gmail.com>
2020-06-12 08:48:48 +0200

    reverted changes

--- bin/picca_export_cross_covariance.py
+++ bin/picca_export_cross_covariance.py
@@ -39,80 +39,67 @@ if __name__ == '__main__':
 
     data = {}
 
-    # Read data
-    for index, filename in enumerate([args.data1, args.data2]):
-        hdul = fitsio.FITS(filename)
-        header = hdul[1].read_header()
-        nside = header['NSIDE']
-        head = hdul[2].read_header()
-        healpix_scheme = header['HLPXSCHM']
-        xi = np.array(hdul[2]['DA'][:])
-        weights = np.array(hdul[2]['WE'][:])
-        healpix_list = np.array(hdul[2]['HEALPID'][:])
-        data[index] = {
-            'DA': xi,
-            'WE': weights,
-            'HEALPID': healpix_list,
-            'NSIDE': nside,
-            'HLPXSCHM': healpix_scheme
-        }
-        hdul.close()
+    ### Read data
+    for i,p in enumerate([args.data1,args.data2]):
+        h = fitsio.FITS(p)
+        head = h[1].read_header()
+        nside = head['NSIDE']
+        head = h[2].read_header()
+        scheme = head['HLPXSCHM']
+        da  = sp.array(h[2]['DA'][:])
+        weights  = sp.array(h[2]['WE'][:])
+        hep = sp.array(h[2]['HEALPID'][:])
+        data[i] = {'DA':da, 'WE':weights, 'HEALPID':hep, 'NSIDE':nside, 'HLPXSCHM':scheme}
+        h.close()
 
-    # exit if NSIDE1 != NSIDE2
-    if data[0]['NSIDE'] != data[1]['NSIDE']:
-        userprint(("ERROR: NSIDE are different: {} != "
-                   "{}").format(data[0]['NSIDE'], data[1]['NSIDE']))
+    ### exit if NSIDE1!=NSIDE2
+    if data[0]['NSIDE']!=data[1]['NSIDE']:
+        userprint("ERROR: NSIDE are different: {} != {}".format(data[0]['NSIDE'],data[1]['NSIDE']))
         sys.exit()
-    # exit if HLPXSCHM1 != HLPXSCHM2
-    if data[0]['HLPXSCHM'] != data[1]['HLPXSCHM']:
-        userprint(("ERROR: HLPXSCHM are different: {} != "
-                   "{}").format(data[0]['HLPXSCHM'], data[1]['HLPXSCHM']))
+    ### exit if HLPXSCHM1!=HLPXSCHM2
+    if data[0]['HLPXSCHM']!=data[1]['HLPXSCHM']:
+        userprint("ERROR: HLPXSCHM are different: {} != {}".format(data[0]['HLPXSCHM'],data[1]['HLPXSCHM']))
         sys.exit()
 
-    # Add unshared healpix as empty data
-    for key in sorted(list(data.keys())):
-        key2 = (key + 1) % 2
-        w = np.logical_not(np.in1d(data[key2]['HEALPID'], data[key]['HEALPID']))
-        if w.sum() > 0:
-            new_healpix = data[key2]['HEALPID'][w]
-            num_new_healpix = new_healpix.size
-            num_bins = data[key]['DA'].shape[1]
-            userprint(("Some healpix are unshared in data {}: "
-                       "{}").format(key, new_healpix))
-            data[key]['DA'] = np.append(data[key]['DA'],
-                                        np.zeros((num_new_healpix, num_bins)),
-                                        axis=0)
-            data[key]['WE'] = np.append(data[key]['WE'],
-                                        np.zeros((num_new_healpix, num_bins)),
-                                        axis=0)
-            data[key]['HEALPID'] = np.append(data[key]['HEALPID'], new_healpix)
+    ### Add unshared healpix as empty data
+    for i in sorted(list(data.keys())):
+        j = (i+1)%2
+        w = np.logical_not( sp.in1d(data[j]['HEALPID'],data[i]['HEALPID']) )
+        if w.sum()>0:
+            new_healpix = data[j]['HEALPID'][w]
+            nb_new_healpix = new_healpix.size
+            nb_bins = data[i]['DA'].shape[1]
+            userprint("Some healpix are unshared in data {}: {}".format(i,new_healpix))
+            data[i]['DA']      = sp.append(data[i]['DA'],np.zeros((nb_new_healpix,nb_bins)),axis=0)
+            data[i]['WE']      = sp.append(data[i]['WE'],np.zeros((nb_new_healpix,nb_bins)),axis=0)
+            data[i]['HEALPID'] = sp.append(data[i]['HEALPID'],new_healpix)
 
-    # Sort the data by the healpix values
-    for key in sorted(list(data.keys())):
-        sort = np.array(data[key]['HEALPID']).argsort()
-        data[key]['DA'] = data[key]['DA'][sort]
-        data[key]['WE'] = data[key]['WE'][sort]
-        data[key]['HEALPID'] = data[key]['HEALPID'][sort]
+    ### Sort the data by the healpix values
+    for i in sorted(list(data.keys())):
+        sort = sp.array(data[i]['HEALPID']).argsort()
+        data[i]['DA']      = data[i]['DA'][sort]
+        data[i]['WE']      = data[i]['WE'][sort]
+        data[i]['HEALPID'] = data[i]['HEALPID'][sort]
 
-    # Append the data
-    xi = np.append(data[0]['DA'], data[1]['DA'], axis=1)
-    weights = np.append(data[0]['WE'], data[1]['WE'], axis=1)
+    ### Append the data
+    da  = sp.append(data[0]['DA'],data[1]['DA'],axis=1)
+    weights  = sp.append(data[0]['WE'],data[1]['WE'],axis=1)
 
-    # Compute the covariance
-    covariance = compute_cov(xi, weights)
+    ### Compute the covariance
+    covariance = compute_cov(da,weights)
 
-    # Get the cross-covariance
-    num_bins = data[0]['DA'].shape[1]
-    cross_covariance = covariance.copy()
-    cross_covariance = cross_covariance[:, num_bins:]
-    cross_covariance = cross_covariance[:num_bins, :]
+    ### Get the cross-covariance
+    size1 = data[0]['DA'].shape[1]
+    cross_co = covariance.copy()
+    cross_co = cross_co[:,size1:]
+    cross_co = cross_co[:size1,:]
 
     ### Get the cross-correlation
-    var = np.diagonal(covariance)
-    cor = covariance / np.sqrt(var * var[:, None])
-    cross_correlation = cor.copy()
-    cross_correlation = cross_correlation[:, num_bins:]
-    cross_correlation = cross_correlation[:num_bins, :]
+    var = sp.diagonal(covariance)
+    cor = covariance/sp.sqrt(var*var[:,None])
+    cross_cor = cor.copy()
+    cross_cor = cross_cor[:,size1:]
+    cross_cor = cross_cor[:size1,:]
 
     ### Test if valid
     try:
@@ -121,9 +108,6 @@ if __name__ == '__main__':
         userprint('WARNING: Matrix is not positive definite')
 
     ### Save
-    results = fitsio.FITS(args.out, 'rw', clobber=True)
-    results.write([cross_covariance, cross_correlation],
-                  names=['CO', 'COR'],
-                  comment=['Covariance matrix', 'Correlation matrix'],
-                  extname='COVAR')
-    results.close()
+    h = fitsio.FITS(args.out,'rw',clobber=True)
+    h.write([cross_co,cross_cor],names=['CO','COR'],comment=['Covariance matrix','Correlation matrix'],extname='COVAR')
+    h.close()
