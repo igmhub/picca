d242b04d9af5dbbe2c2f58cf67563e969ad9c11f
Ignasi Pérez Ràfols <iprafols@gmail.com>
2021-04-20 11:13:10 +0200

    added missing Forest import

--- py/picca/delta_extraction/expected_fluxes/dr16_expected_flux.py
+++ py/picca/delta_extraction/expected_fluxes/dr16_expected_flux.py
@@ -4,6 +4,7 @@ from scipy.interpolate import interp1d
 from multiprocessing import Pool
 import fitsio
 
+from picca.delta_extraction.astronomical_objects.forest import Forest
 from picca.delta_extraction.expected_flux import ExpectedFlux
 from picca.delta_extraction.errors import ExpectedFluxError
 
