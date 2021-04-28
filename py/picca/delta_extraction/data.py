31fede9ac67ea62edd577025f363a93365a060df
Ignasi Pérez Ràfols <iprafols@gmail.com>
2021-04-28 13:26:41 +0200

    simplofied filtering conditions

--- py/picca/delta_extraction/data.py
+++ py/picca/delta_extraction/data.py
@@ -71,10 +71,7 @@ class Data:
         self.logger.progress(f"Input sample has {len(self.forests)} forests")
         remove_indexs = []
         for index, forest in enumerate(self.forests):
-            if ((Forest.wave_solution == "log" and
-                 len(forest.log_lambda) < self.min_num_pix) or
-                    (Forest.wave_solution == "lin" and
-                     len(forest.lambda_) < self.min_num_pix)):
+            if forest.flux.size < self.min_num_pix):
                 self.logger.progress(
                     f"Rejected forest with thingid {forest.thingid} "
                     "due to forest being too short")
