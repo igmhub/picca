22f7b7f9a0f5faba262f008b756e05ef0ddbcd0b
arkel1 <47539636+arkel1@users.noreply.github.com>
2021-10-04 02:28:14 -0500

    Update blinding_a.py
    Changed l_grid, l_map, l_* to lambda_* accordingto request. This was applied to all var names in the file.
    Chaged outputx and other variants to x_out (also for flux and ivar in resampling functions).
    Deleted functions to mute/unmute print std_out.

--- py/picca/blinding_a.py
+++ py/picca/blinding_a.py
@@ -5,22 +5,6 @@ import sys, os  # requiered to mute Cosmo and dont print the
                 #   cosmology change on screen
 
 
-# disable print to null to avout cosmo function outputs with O_m values
-def blockPrint():
-    """Redirect stdout to null (mute print to screen functions)
-        until enablePrint() is called
-
-    """
-    sys.stdout = open(os.devnull, 'w')
-
-# Restore
-def enablePrint():
-    """Redirect stdout to __stdout__ (Unmute print to screen functions)
-        after enablePrint() is called
-
-    """
-    sys.stdout = sys.__stdout__
-
 def calcMaps(scale=.95, Om=0.315):
     """Initialize class and compute the conversion maps
 
@@ -32,12 +16,11 @@ def calcMaps(scale=.95, Om=0.315):
     Returns:
         z_grid, z_map: input and output values of mapping functions
             to interpolate and blind the qso redshift value: z' = z+zmap(z)
-        l_grid, l_map: input and output values of mapping functions
-            to interpolate and blind the forest lambda values: l'= l*lmap(l)
+        lambda_grid, lambda_map: input and output values of mapping functions
+            to interpolate and blind the forest lambda values: 
+            lambda'= lambda*lambda_map(lambda)
     """
 
-
-    blockPrint() # mute print output for Cosmo function
     ###### Definition of the fiducial cosmological model
     
     fid_Om = Om
@@ -61,40 +44,38 @@ def calcMaps(scale=.95, Om=0.315):
     lambda_abs = ABSORBER_IGM['LYA']
     
     #######
-    
-    enablePrint()# Unmute print output for Cosmo function
-    
-    zmax = 10.
-    l_max = ( lambda_abs * (zmax + 1) ) - 1.3
-    ll = np.log10( np.linspace(lambda_abs + .5, l_max, 10000) )
-    z = 10**ll/lambda_abs-1.
+        
+    z_max = 10.
+    lambda_max = ( lambda_abs * (z_max + 1) ) - 1.3
+    log_lambda = np.log10( np.linspace(lambda_abs + .5, lambda_max, 10000) )
+    z = 10**log_lambda/lambda_abs-1.
     
     r_comov = cosmo_m.get_r_comov(z)
     r_comov2 = cosmo_m2.get_r_comov(z)
     
-    znew = resample_flux(r_comov, r_comov2, z)
-    zmask = ( z <= 10 )
+    z_prime = resample_flux(r_comov, r_comov2, z)
+    z_mask = ( z <= 10 )
 
-    z_grid = z[zmask]
-    znew = znew[zmask]
-    r_comov = r_comov[zmask]
+    z_grid = z[z_mask]
+    z_prime = z_prime[z_mask]
+    r_comov = r_comov[z_mask]
     r_comov2 = r_comov2[zmask]
-    ll = ll[zmask]
+    log_lambda_grid = log_lambda[zmask]
     
-    llnew = np.log10( lambda_abs * ( znew + 1 ) )
-    z_map = -znew + z
-    l_grid = 10**ll
-    l_map = l_grid / (10**llnew)
+    log_lambda_prime = np.log10( lambda_abs * ( z_prime + 1 ) )
+    z_map = -z_prime + z
+    lambda_grid = 10**log_lambda_grid
+    lambda_map = lambda_grid / (10**log_lambda_prime)
     
-    return z_grid, z_map, l_grid, l_map
+    return z_grid, z_map, lambda_grid, lambda_map
 
-def resample_flux(xout, x, flux, ivar=None, extrapolate=False):
+def resample_flux(x_out, x, flux, ivar=None, extrapolate=False):
     """Function to do a weighted resampling of fluxes into a new
         absissa array
         Taken from desispec.interpolation::
 
     Args:
-        xout: abscissa array for which the flux will be resampled
+        x_out: abscissa array for which the flux will be resampled
         x: original abscissa
         flux: original flux
         ivar: inverse variance weighting of the original flux,
@@ -102,45 +83,45 @@ def resample_flux(xout, x, flux, ivar=None, extrapolate=False):
         extrapolate: if true borders will be extrapolated to 
             "improve" the edge resampling
     Returns:
-        outflux: new flux resampled to xout, i.e. flux(xout)
-        outivar: -only if ivar given as arg- new ivar values 
+        out_flux: new flux resampled to xout, i.e. flux(xout)
+        out_ivar: -only if ivar given as arg- new ivar values 
             for outflux
 
     """
     if ivar is None:
-        return _unweighted_resample(xout, x, flux, extrapolate=extrapolate)
+        return _unweighted_resample(x_out, x, flux, extrapolate=extrapolate)
     else:
         if extrapolate :
             raise ValueError("Cannot extrapolate ivar. Either set ivar=None and extrapolate=True or the opposite")
-        a = _unweighted_resample(xout, x, flux*ivar, extrapolate=False)
-        b = _unweighted_resample(xout, x, ivar, extrapolate=False)
+        a = _unweighted_resample(x_out, x, flux*ivar, extrapolate=False)
+        b = _unweighted_resample(x_out, x, ivar, extrapolate=False)
         mask = (b>0)
-        outflux = np.zeros(a.shape)
-        outflux[mask] = a[mask] / b[mask]
+        out_flux = np.zeros(a.shape)
+        out_flux[mask] = a[mask] / b[mask]
         dx = np.gradient(x)
-        dxout = np.gradient(xout)
-        outivar = _unweighted_resample(xout, x, ivar/dx)*dxout
+        dx_out = np.gradient(x_out)
+        out_ivar = _unweighted_resample(x_out, x, ivar/dx)*dx_out
 
-        return outflux, outivar
+        return out_flux, out_ivar
 
-def _unweighted_resample(output_x,input_x,input_flux_density, extrapolate=False) :
+def _unweighted_resample(x_out, x, flux_density, extrapolate=False) :
     """Function to do a unweighted resampling of fluxes into a new
         absissa array, works as a interpolation
         Taken from desispec.interpolation
 
     Args:
-        output_x: abscissa array for which the flux will be resampled
-        input_x: original abscissa
-        input_flux_density: original flux
+        x_out: abscissa array for which the flux will be resampled
+        x: original abscissa
+        flux_density: original flux density
         extrapolate: if true borders will be extrapolated to 
             "improve" the edge resampling
     Returns:
         histogram: new flux resampled to output_x, i.e. 
-            input_flux_density(output_x)
+            flux_density(x_out)
     """
-    ix=input_x
-    iy=input_flux_density
-    ox=output_x
+    ix=x
+    iy=flux_density
+    ox=x_out
 
     # boundary of output bins
     bins=np.zeros(ox.size+1)
@@ -180,7 +161,7 @@ def _unweighted_resample(output_x,input_x,input_flux_density, extrapolate=False)
 
     return np.histogram(trapeze_centers, bins=bins, weights=trapeze_integrals)[0] / binsize
 
-def blindData(data, z_grid, z_map, l_grid, l_map):
+def blindData(data, z_grid, z_map, lambda_grid, lambda_map):
     """Ads AP blinding (strategy A) to the deltas data
 
     Args:
@@ -188,58 +169,59 @@ def blindData(data, z_grid, z_map, l_grid, l_map):
             A dictionary with the forests in each healpix
         z_grid, z_map: input and output values of mapping functions
             to interpolate and blind the qso redshift value: z' = z+zmap(z)
-        l_grid, l_map: input and output values of mapping functions
-            to interpolate and blind the forest lambda values: l' = l*lmap(l)
+        lambda_grid, lambda_map: input and output values of mapping functions
+            to interpolate and blind the forest lambda values: 
+            lambda' = lambda*lambda_map(lambda)
 
     Returns:
         The following variables:
         data: dict
             A dictionary with the blinded forests and in each healpix
     """
-    lpix = []
+    lambda_pix = []
 
     for healpix in sorted(list(data.keys())):
         for forest in data[healpix]:
             # Z QSO ap shift
-            Za = forest.z_qso
-            Z_rebin = np.interp( Za, z_grid, z_map )
-            forest.z_qso = Za + Z_rebin
+            z_qso = forest.z_qso # may  be used later depending on the new grid
+            z_rebin = np.interp( z_qso, z_grid, z_map )
+            forest.z_qso = z_qso + z_rebin
             
             # QSO forest ap shift with interval conservation
-            l = 10**( forest.log_lambda )
-            l_map_rebin = resample_flux( l, l_grid, l_map )
+            lambda = 10**( forest.log_lambda )
+            lambda_map_rebin = resample_flux( lambda, lambda_grid, lambda_map )
 
-            l_rebin = l_map_rebin*l     #this new vector is not linearly spaced in lambda
-            # creating new linear loglam interval to save all data
-            l2 = 10**( np.arange(np.log10( np.min(l_rebin)), np.log10(np.max(l_rebin)) , forest.delta_log_lambda)   )
-            # l2 = l-( l[0]-l_rebin[0] )  #to use original picca interval, just shifting it (more data loss)
-            flux, ivar = resample_flux(l2, l_rebin, forest.flux, ivar=forest.ivar )   #rebining while conserving flux
-            cont = resample_flux(l2, l_rebin, forest.cont )         #rebining continuum
+            lambda_rebin = lambda_map_rebin*lambda     # this new lambda' vector is not linearly spaced in lambda
+            # creating new linear loglam interval to save lambda'
+            lambda_prime = 10**( np.arange(np.log10( np.min(lambda_rebin)), np.log10(np.max(lambda_rebin)) , forest.delta_log_lambda)   )
+            # lambda_prime = lambda-( lambda[0]-lambda_rebin[0] )  #to use original picca interval, just shifting it to store lambda' (more data loss)
+            flux, ivar = resample_flux(lambda_prime, lambda_rebin, forest.flux, ivar=forest.ivar )   #rebining to lambda' while conserving flux
+            cont = resample_flux(lambda_prime, lambda_rebin, forest.cont )     # rebining continuum to lambda'
             
-            # to calc pixel loss
-            #lrebinrest = np.log10( l_rebin / ( 1 + forest.z_qso ) )
-            #lrebinmask = ( lrebinrest > forest.log_lambda_min_rest_frame) & ( lrebinrest < forest.log_lambda_max_rest_frame) & (np.log10(l_rebin) > forest.log_lambda_min ) & (  (l_rebin) < ( 10**forest.log_lambda_max - 1) )
 
-            # 1 to mask data: first two for restframe lya range, 
-            #   last two for prep_del obs frame stacks, only needed when
-            #   blinding is before/during continuum fitting, will induce pixel loss
+            # 1 to mask data: first two conditionals to cut new data to restframe lya range, 
+            #  last two conditionals to cut data for prep_del stacks in observed frame, only needed 
+            #  when blinding is before/during continuum fitting, will induce pixel loss
             if 0:
-               lnrest = np.log10(   l2 / (1+forest.z_qso)   )
-               lmask = (lnrest > forest.log_lambda_min_rest_frame) & (lnrest < forest.log_lambda_max_rest_frame) & (np.log10(l2) > forest.log_lambda_min ) & (  (l2) < ( 10**forest.log_lambda_max - 1) )
-               forest.log_lambda = np.log10( l2[lmask]  )
-               forest.flux = flux[lmask]
-               forest.ivar = ivar[lmask]
+               # to calc pixel loss
+               #lambda_rebin_rest = np.log10( lambda_rebin / ( 1 + forest.z_qso ) )
+               #lambda_rebin_mask = ( lambda_rebin_rest > forest.log_lambda_min_rest_frame) & ( lambda_rebin_rest < forest.log_lambda_max_rest_frame) & (np.log10(lambda_rebin) > forest.log_lambda_min ) & (  (lambda_rebin) < ( 10**forest.log_lambda_max - 1) )
+               lambda_prime_rest = np.log10(   lambda_prime / (1+forest.z_qso)   )   # using new z_qso value
+               lambda_mask = (lambda_prime_rest > forest.log_lambda_min_rest_frame) & (lambda_prime_rest < forest.log_lambda_max_rest_frame) & (np.log10(lambda_prime) > forest.log_lambda_min ) & (  (lambda_prime) < ( 10**forest.log_lambda_max - 1) )
+               forest.log_lambda = np.log10( lambda_prime[lambda_mask]  )
+               forest.flux = flux[lambda_mask]
+               forest.ivar = ivar[lambda_mask]
             else:
-               forest.log_lambda = np.log10( l2 )
+               forest.log_lambda = np.log10( lambda_prime )
                forest.flux = flux
                forest.ivar = ivar
                forest.cont = cont
                if forest.mean_expected_flux_frac is not None:
-                  forest.mean_expected_flux_frac = resample_flux(l2, l_rebin, forest.mean_expected_flux_frac  )
+                  forest.mean_expected_flux_frac = resample_flux(lambda_prime, lambda_rebin, forest.mean_expected_flux_frac  )
             
-            # pixel loss
-            #diff = len(l_rebin) - sum(lrebinmask)
-            #lpix.append( np.hstack(( Za, forest.z_qso, diff, len(l_rebin) ))  )
-    # pixel loss
-    #np.save('~/losspixels_picca', lpix)        
+            # to save pixel loss
+            #pixel_diff = len(lambda_rebin) - sum(lambda_rebin_mask)
+            #lambda_pix.append( np.hstack(( z_qso, forest.z_qso, pixel_diff, len(lambda_rebin) ))  )
+    # to save pixel loss
+    #np.save('~/losspixels_picca', lambda_pix)        
     return data
