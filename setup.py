c7571ac2b599bd975aa4a7b0497fb5151958b56b
Michael Walther <Waelthus@users.noreply.github.com>
2021-12-02 14:00:36 +0100

    adding info to setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,21 @@ scripts = sorted(glob.glob('bin/picca*'))
 
 description = "Package for Igm Cosmological-Correlations Analyses"
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 exec(open('py/picca/_version.py').read())
 version = __version__
 
 setup(name="picca",
     version=version,
     description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/igmhub/picca",
-    author="Nicolas Busca, Helion du Mas des Bourboux et al",
-    author_email="nbusca@lpnhe.in2p3.fr",
+    author="The eBOSS and DESI collaboration Lya forest working groups",
+    author_email="desi-lya@desi.lbl.gov",
     packages=['picca','picca.fitter2','picca.bin'],
     package_dir = {'': 'py'},
     package_data = {'picca': ['fitter2/models/*/*.fits']},
