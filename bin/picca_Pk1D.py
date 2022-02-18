a489b1f13c99abe4cf8c9aeee5730da250bfa1e7
Michael Walther <Waelthus@users.noreply.github.com>
2022-02-18 11:14:00 +0100

    start adding multiprocessing

--- bin/picca_Pk1D.py
+++ bin/picca_Pk1D.py
@@ -14,6 +14,42 @@ from picca.pk1d import (compute_correction_reso, compute_correction_reso_matrix,
                         compute_pk_raw, fill_masked_pixels, rebin_diff_noise,
                         split_forest)
 from picca.utils import userprint
+from multiprocessing import Pool
+
+
+
+def check_linear_binning(delta):
+    """checks if the wavelength binning is linear or log this is stable against masking
+
+    Args:
+        delta (Delta): delta class as read with Delta.from_...
+
+    Raises:
+        ValueError: Raised if binning is neither linear nor log, or if delta.log_lambda was actually wavelength
+
+    Returns:
+        linear_binning (bool): boolean telling the binning_type
+        delta_lam (float): size of a wavelength bin in the right unit
+    """
+
+    diff_lambda = np.diff(10**delta.log_lambda)
+    diff_log_lambda = np.diff(delta.log_lambda)
+    q5_lambda, q25_lambda = np.percentile(diff_lambda,[5,25])
+    q5_log_lambda, q25_log_lambda = np.percentile(diff_log_lambda,[5,25])
+    if (q25_lambda-q5_lambda)<1e-6:
+        #we can assume linear binning for this case
+        linear_binning=True
+        delta_lam = np.median(diff_lambda)
+    elif (q25_log_lambda-q5_log_lambda)<1e-6 and q5_log_lambda<0.01:
+        #we can assume log_linear binning for this case
+        linear_binning=False
+        delta_lam = np.median(diff_log_lambda)
+    elif (q5_log_lambda>=0.01):
+        raise ValueError("Could not figure out if linear or log wavelength binning was used, probably submitted lambda as log_lambda")
+    else:
+        raise ValueError("Could not figure out if linear or log wavelength binning was used")
+
+    return linear_binning, delta_lam
 
 def main(cmdargs):
     # pylint: disable-msg=too-many-locals,too-many-branches,too-many-statements
@@ -114,6 +150,12 @@ def main(cmdargs):
               'redshift of the forest pixels'))
 
     #additional options
+    parser.add_argument('--num-processors',
+                        type=int,
+                        default=1,
+                        required=False,
+                        help='Number of processors to use for computation')
+
     parser.add_argument(
         '--num-noise-exp',
         default = 10,
@@ -146,6 +188,10 @@ def main(cmdargs):
     # initialize randoms
     np.random.seed(4)
 
+
+    if args.num_processors>1:
+        pool=Pool(args.num_processors)
+
     # loop over input files
     for index, file in enumerate(files):
         if index % 1 == 0:
@@ -162,32 +208,30 @@ def main(cmdargs):
             ascii_file = open(file, 'r')
             deltas = [Delta.from_ascii(line) for line in ascii_file]
 
-        #add the check for linear binning on first spectrum of first file only
-        if index==0:
-            delta = deltas[0]
-            diff_lambda = np.diff(10**delta.log_lambda)
-            diff_log_lambda = np.diff(delta.log_lambda)
-            q25_lambda, q75_lambda = np.percentile(diff_lambda,[25,75])
-            q25_log_lambda, q75_log_lambda = np.percentile(diff_lambda,[25,75])
-            if (q75_lambda-q25_lambda)<1e-6:
-                #we can assume linear binning for this case
-                linear_binning=True
-                delta_lambda = np.median(diff_lambda)
-            elif (q75_log_lambda-q25_log_lambda)<1e-6:
-                #we can assume log_linear binning for this case
-                linear_binning=False
-                delta_log_lambda = np.median(diff_log_lambda)
+        #add the check for linear binning on first spectrum only (assuming homogeneity within the file)
+        delta = deltas[0]
+        if index=0:
+            linear_binning, delta_lam = check_linear_binning(delta)
+            if linear_binning:
+                userprint("\n Using linear binning, results will have units of AA")
+                delta_lambda = delta_lam
             else:
-                raise ValueError("Could not figure out if linear or log wavelength binning was used")
+                userprint("\n Using log binning, results will have units of km/s")
+                delta_log_lambda = delta_lam
+        else:
+            linear_binning2, delta_lam2 = check_linear_binning(delta)
+            try:
+                assert linear_binning2 == linear_binning
+                assert delta_lam2 == delta_lam
+            except AssertionError:
+                raise("inhomogeneous wavelength binning between input files")
 
 
         num_data += len(deltas)
         userprint("\n ndata =  ", num_data)
         results = None
 
-        # loop over deltas
-        for delta in deltas:
-
+        def process_file(delta):
             # Selection over the SNR and the resolution
             if (delta.mean_snr <= args.SNR_min or
                     delta.mean_reso >= args.reso_max):
@@ -212,6 +256,8 @@ def main(cmdargs):
                                         delta.log_lambda, delta.delta,
                                         delta.exposures_diff, delta.ivar,
                                         first_pixel_index)
+
+            pk_list=[]
             for index2 in range(num_parts):
 
                 # rebin exposures_diff spectrum
@@ -247,7 +293,6 @@ def main(cmdargs):
                                constants.speed_light / 1000.)
                 correction_reso = compute_correction_reso(
                     delta_pixel, delta.mean_reso, k)
-
                 # Compute 1D Pk
                 if args.noise_estimate == 'pipeline':
                     pk = (pk_raw - pk_noise) / correction_reso
@@ -262,6 +307,18 @@ def main(cmdargs):
                     mean_pk_diff = (sum(pk_diff[selection]) /
                                     float(len(pk_diff[selection])))
                     pk = (pk_raw - mean_pk_diff) / correction_reso
+                pk_list.append([k, pk_raw, pk_noise, pk_diff, correction_reso, pk])
+            return pk_list
+
+
+        if args.num_processes>1:
+            pk_list_of_lists=pool.map(process_file(delta))
+
+        else:
+            pk_list_of_lists=[process_file(delta)]
+
+        for delta, pk_list in zip(deltas, pk_list_of_lists):
+            for k, pk_raw, pk_noise, pk_diff, correction_reso, pk in pk_list:
 
                 # save in fits format
                 if args.out_format == 'fits':
@@ -341,8 +398,13 @@ def main(cmdargs):
                                       header=header,
                                       comment=comments,
                                       units=units)
+
+
+
         if (args.out_format == 'fits' and results is not None):
             results.close()
+
+        
 
     userprint("all done ")
 
