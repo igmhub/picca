53b1717e6546615d394048e8132bc236076c257f
Michael Walther <Waelthus@users.noreply.github.com>
2022-02-18 18:49:11 +0100

    add reading of reso matrix for tile

--- py/picca/delta_extraction/data_catalogues/desi_tile.py
+++ py/picca/delta_extraction/data_catalogues/desi_tile.py
@@ -289,6 +289,7 @@ class DesiTile(DesiData):
 
                         args["exposures_diff"] = exposures_diff
                         args["reso"] = reso_in_km_per_s
+                        args["resolution_matrix"] = reso_sum
                         forest = DesiPk1dForest(**args)
                     else:
                         raise DataError("Unkown analysis type. Expected 'BAO 3D'"
