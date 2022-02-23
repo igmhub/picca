change spectral_resolution_desi to give back correct resolution in km/s, pipe both resolution in units km/s and pixels through until the end

py/picca/delta_extraction/astronomical_objects/pk1d_forest.py
@@ -176,6 +176,7 @@ class Pk1dForest(Forest):
 
         # compute mean quality variables
         self.mean_reso = self.reso.mean()
+        self.mean_reso_pix = self.reso_pix.mean()
         if Forest.wave_solution == "log":
             self.mean_z = (
                 (np.power(10., self.log_lambda[len(self.log_lambda) - 1]) +
@@ -214,6 +215,8 @@ class Pk1dForest(Forest):
             Forest.mask_fields += ["exposures_diff"]
         if "reso" not in Forest.mask_fields:
             Forest.mask_fields += ["reso"]
+        if "reso_pix" not in Forest.mask_fields:
+            Forest.mask_fields += ["reso_pix"]
         if self.resolution_matrix is not None:
             if self.resolution_matrix.shape[1] != self.flux.shape[0]:
                 raise AstronomicalObjectError(
@@ -247,6 +250,7 @@ class Pk1dForest(Forest):
         self.exposures_diff = np.append(self.exposures_diff,
                                         other.exposures_diff)
         self.reso = np.append(self.reso, other.reso)
+        self.reso_pix = np.append(self.reso_pix, other.reso_pix)
 
         if self.resolution_matrix is not None:
             if other.resolution_matrix.size>0 and self.resolution_matrix.size>0:
@@ -278,13 +282,15 @@ class Pk1dForest(Forest):
         """
         cols, names, units, comments = super().get_data()
 
-        cols += [self.ivar, self.exposures_diff]
-        names += ["IVAR", "DIFF"]
+        cols += [self.ivar, self.exposures_diff, self.reso, self.reso_pix]
+        names += ["IVAR", "DIFF", "RESO", "RESO_PIX"]
         comments += [
             "Inverse variance. Check input spectra for units",
-            "Difference. Check input spectra for units"
+            "Difference. Check input spectra for units",
+            "Resolution estimate for each pixel in units of km/s"
+            "Resolution estimate for each pixel in units of pixel size"
         ]
-        units += ["Flux units", "Flux units"]
+        units += ["Flux units", "Flux units", "", ""]
 
         if self.resolution_matrix is not None:
             #transposing here is necessary to store in fits file
@@ -318,7 +324,12 @@ class Pk1dForest(Forest):
             {
                 'name': 'MEANRESO',
                 'value': self.mean_reso,
-                'comment': 'Mean resolution'
+                'comment': 'Mean resolution (km/s)'
+            },
+            {
+                'name': 'MEANRESO_PIX',
+                'value': self.mean_reso_pix,
+                'comment': 'Mean resolution (pixels)'
             },
         ]
 
@@ -359,6 +370,7 @@ class Pk1dForest(Forest):
         if len(rebin_ivar) == 0:
             self.exposures_diff = np.array([])
             self.reso = np.array([])
+            self.reso_pix = np.array([])
             if self.resolution_matrix is not None:
                 self.resolution_matrix = np.array([[]])
             return [], [], [], [], []
@@ -366,6 +378,7 @@ class Pk1dForest(Forest):
         # apply mask due to cuts in bin
         self.exposures_diff = self.exposures_diff[w1]
         self.reso = self.reso[w1]
+        self.reso_pix = self.reso_pix[w1]
         if self.resolution_matrix is not None:
             self.resolution_matrix = self.resolution_matrix[:, w1]
 
@@ -373,13 +386,16 @@ class Pk1dForest(Forest):
         # rebin exposures_diff and reso
         rebin_exposures_diff = np.zeros(bins.max() + 1)
         rebin_reso = np.zeros(bins.max() + 1)
+        rebin_reso_pix = np.zeros(bins.max() + 1)
         rebin_exposures_diff_aux = np.bincount(bins,
                                                weights=orig_ivar[w1] *
                                                self.exposures_diff)
         rebin_reso_aux = np.bincount(bins, weights=orig_ivar[w1] * self.reso)
+        rebin_reso_pix_aux = np.bincount(bins, weights=orig_ivar[w1] * self.reso_pix)
         rebin_exposures_diff[:len(rebin_exposures_diff_aux
                                  )] += rebin_exposures_diff_aux
         rebin_reso[:len(rebin_reso_aux)] += rebin_reso_aux
+        rebin_reso_pix[:len(rebin_reso_pix_aux)] += rebin_reso_pix_aux
 
         if self.resolution_matrix is not None:
             rebin_reso_matrix_aux = np.zeros((self.resolution_matrix.shape[0], bins.max() + 1))
@@ -389,11 +405,13 @@ class Pk1dForest(Forest):
         # apply mask due to rebinned inverse vairane
         self.exposures_diff = rebin_exposures_diff[w2] / rebin_ivar[w2]
         self.reso = rebin_reso[w2] / rebin_ivar[w2]
+        self.reso_pix = rebin_reso_pix[w2] / rebin_ivar[w2]
         if self.resolution_matrix is not None:
             self.resolution_matrix = rebin_reso_matrix_aux[:, w2] / rebin_ivar[np.newaxis, w2]
 
         # finally update control variables
         self.mean_reso = self.reso.mean()
+        self.mean_reso_pix = self.reso_pix.mean()
         if Forest.wave_solution == "log":
             self.mean_z = (
                 (np.power(10., self.log_lambda[len(self.log_lambda) - 1]) +

py/picca/delta_extraction/data_catalogues/desi_tile.py
@@ -285,17 +285,12 @@ class DesiTile(DesiData):
                         reso_in_pix, reso_in_km_per_s = np.real(
                             spectral_resolution_desi(reso_sum,
                                                      spec['WAVELENGTH']))
-                        if Forest.wave_solution == "lin":
-                            reso_in_AA = reso_in_pix * (spec['WAVELENGTH'][1]-spec['WAVELENGTH'][0])
-                            #TODO: might be useful to treat this more properly, but probably only needed for debugging anyway
-                        else:
-                            reso_in_AA = None
                         exposures_diff = np.zeros(spec['log_lambda'].shape)
 
                         args["exposures_diff"] = exposures_diff
                         args["reso"] = reso_in_km_per_s
                         args["resolution_matrix"] = reso_sum
-                        args["reso_AA"] = reso_in_AA
+                        args["reso_pix"] = reso_in_pix
 
                         forest = DesiPk1dForest(**args)
                     else:

py/picca/delta_extraction/utils_pk1d.py
@@ -154,8 +154,12 @@ def spectral_resolution_desi(reso_matrix, lambda_):
     reso_in_km_per_s: array
     The spectral resolution
     """
-    delta_log_lambda = ((lambda_[-1] - lambda_[0]) /
-                    float(len(lambda_) - 1))
+    delta_log_lambda = np.diff(np.log(lambda_))
+    #note that this would be the same result as before (except for the missing bug) in
+    #case of log-uniform binning, but for linear binning pixel size chenges wrt lambda
+    delta_log_lambda = np.append(delta_log_lambda,
+                                 [delta_log_lambda[-1]+
+                                  (delta_log_lambda[-1]-delta_log_lambda[-2])])
     reso = np.clip(reso_matrix, 1.0e-6, 1.0e6)
     rms_in_pixel = (
         np.sqrt(1.0 / 2.0 / np.log(
@@ -167,7 +171,7 @@ def spectral_resolution_desi(reso_matrix, lambda_):
         np.sqrt(4.0 / 2.0 / np.log(
             reso[len(reso) // 2][:] / reso[len(reso) // 2 + 2][:]))) / 4.0
 
-    avg_reso_in_km_per_s = (rms_in_pixel * SPEED_LIGHT * delta_log_lambda *
+    reso_in_km_per_s = (rms_in_pixel * SPEED_LIGHT * delta_log_lambda *
                             np.log(10.0))
 
-    return rms_in_pixel, avg_reso_in_km_per_s
+    return rms_in_pixel, reso_in_km_per_s