fix the diff calculation (eg for mocks)

py/picca/delta_extraction/data_catalogues/desi_healpix.py
@@ -185,6 +185,7 @@ class DesiHealpix(DesiData):
         colors = ["B", "R"]
         if "Z_FLUX" in hdul:
             colors.append("Z")
+
         for color in colors:
             spec = {}
             try:
@@ -196,7 +197,12 @@ class DesiHealpix(DesiData):
                 for key in ["FLUX", "IVAR"]:
                     spec[key][w] = 0.
                 if self.analysis_type == "PK 1D":
-                    spec['TEFF_LYA'] = 11.80090901380597 * hdul['SCORES'][f'TSNR2_LYA_{color}'].read()
+                    if "SCORES" in hdul:
+                        spec['TEFF_LYA'] = 11.80090901380597 * hdul['SCORES'][f'TSNR2_LYA_{color}'].read()
+                    else:
+                        spec['TEFF_LYA'] = np.ones(spec["FLUX"].shape[0])
+                        self.logger.info("SCORES are missing, Teff information (and thus DIFF) will be garbage")
+
                     if f"{color}_RESOLUTION" in hdul:
                         spec["RESO"] = hdul[f"{color}_RESOLUTION"].read()
                     else:

py/picca/delta_extraction/data_catalogues/desi_tile.py
@@ -225,7 +225,11 @@ class DesiTile(DesiData):
                     spec['IVAR'] = (hdul[f'{color}_IVAR'].read() *
                                     (hdul[f'{color}_MASK'].read() == 0))
                     if self.analysis_type == "PK 1D":
-                        spec['TEFF_LYA'] = 11.80090901380597 * hdul['SCORES'][f'TSNR2_LYA_{color}'].read()
+                        if "SCORES" in hdul:
+                            spec['TEFF_LYA'] = 11.80090901380597 * hdul['SCORES'][f'TSNR2_LYA_{color}'].read()
+                        else:
+                            spec['TEFF_LYA'] = np.ones(spec["FLUX"].shape[0])
+                            self.logger.info("SCORES are missing, Teff information (and thus DIFF) will be garbage")
                         if f"{color}_RESOLUTION" in hdul:
                             spec["RESO"] = hdul[f"{color}_RESOLUTION"].read()
                         else: