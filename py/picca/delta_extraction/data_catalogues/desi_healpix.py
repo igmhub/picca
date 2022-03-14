26fc5de786f12a9605b49f1c783f02da6812bf0c
Michael Walther <Waelthus@users.noreply.github.com>
2022-03-14 11:42:10 +0100

    fix position of parse config

--- py/picca/delta_extraction/data_catalogues/desi_healpix.py
+++ py/picca/delta_extraction/data_catalogues/desi_healpix.py
@@ -70,8 +70,9 @@ class DesiHealpix(DesiData):
         Parsed options to initialize class
         """
         self.logger = logging.getLogger(__name__)
-        super().__init__(config)
         self.__parse_config(config)
+
+        super().__init__(config)
 
     def __parse_config(self, config):
         """Parse the configuration options
