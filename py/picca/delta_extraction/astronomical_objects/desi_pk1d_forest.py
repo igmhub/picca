0aab554d73bc3dbbe8c4cdc58f78eac754766fdd
Michael Walther <Waelthus@users.noreply.github.com>
2022-04-11 14:35:23 +0200

    missing brackets

--- py/picca/delta_extraction/astronomical_objects/desi_pk1d_forest.py
+++ py/picca/delta_extraction/astronomical_objects/desi_pk1d_forest.py
@@ -103,11 +103,11 @@ class DesiPk1dForest(DesiForest, Pk1dForest):
                 smallershape = np.min([self.resolution_matrix.shape[0],other.resolution_matrix.shape[0]])
                 shapediff = largershape - smallershape
                 if self.resolution_matrix.shape[0]==smallershape:
-                    self.resolution_matrix = np.append(np.zeros(shapediff//2,self.resolution_matrix.shape[1]),self.resolution_matrix,axis=0)
-                    self.resolution_matrix = np.append(self.resolution_matrix,np.zeros(shapediff//2,self.resolution_matrix.shape[1]),axis=0)
+                    self.resolution_matrix = np.append(np.zeros([shapediff//2,self.resolution_matrix.shape[1]]),self.resolution_matrix,axis=0)
+                    self.resolution_matrix = np.append(self.resolution_matrix,np.zeros([shapediff//2,self.resolution_matrix.shape[1]]),axis=0)
                 if other.resolution_matrix.shape[0]==smallershape:
-                    other.resolution_matrix = np.append(np.zeros(shapediff//2,other.resolution_matrix.shape[1]),other.resolution_matrix,axis=0)
-                    other.resolution_matrix = np.append(other.resolution_matrix,np.zeros(shapediff//2,other.resolution_matrix.shape[1]),axis=0)
+                    other.resolution_matrix = np.append(np.zeros([shapediff//2,other.resolution_matrix.shape[1]]),other.resolution_matrix,axis=0)
+                    other.resolution_matrix = np.append(other.resolution_matrix,np.zeros([shapediff//2,other.resolution_matrix.shape[1]]),axis=0)
 
             self.resolution_matrix = np.append(self.resolution_matrix,
                                                other.resolution_matrix,
