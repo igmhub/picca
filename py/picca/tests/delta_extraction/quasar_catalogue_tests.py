591f8fb9a2a34298ed7eefba9d3fcc36607f246c
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2022-04-26 15:20:29 +0200

    added further testing

--- py/picca/tests/delta_extraction/quasar_catalogue_tests.py
+++ py/picca/tests/delta_extraction/quasar_catalogue_tests.py
@@ -196,6 +196,17 @@ class QuasarCatalogueTest(AbstractTest):
         self.assertTrue(quasar_catalogue.z_max == 4.288461538461538)
         self.assertTrue(quasar_catalogue.max_num_spec == 2)
 
+        # case: no zmin, cannot compute it
+        config = ConfigParser()
+        config.read_dict({"data": {
+            "z min": 2.0,
+        }})
+        expected_message = (
+            "Missing argument 'z max' required by QuasarCatalogue")
+        with self.assertRaises(QuasarCatalogueError) as context_manager:
+            quasar_catalogue = QuasarCatalogue(config["data"])
+        self.compare_error_message(context_manager, expected_message)
+
     def test_quasar_catalogue_trim_catalogue(self):
         """Test method trim_catalogue from Abstract Class QuasarCatalogue"""
         ra = [0.15, 0.0]
