5d7a4f4ef18c094f631c27e959e86e046d81b06b
Naim Karacayli <ngokselk@gmail.com>
2022-04-28 11:41:18 -0700

    paralel reader class for multiprocessing. forests are merged on the master. Forest.mask_fields are updated before reading files.

--- py/picca/delta_extraction/data_catalogues/desi_healpix.py
+++ py/picca/delta_extraction/data_catalogues/desi_healpix.py
@@ -24,174 +24,15 @@ defaults.update({
 })
 
 
-class DesiHealpix(DesiData):
-    """Reads the spectra from DESI using healpix mode and formats its data as a
-    list of Forest instances.
+# Class to read in parallel
+# Seems lightweight to copy all these 3 arguments
+class ParallelReader(object):
+    def __init__(self, analysis_type, use_non_coadded_spectra, logger):
+        self.logger = logger
+        self.analysis_type = analysis_type
+        self.use_non_coadded_spectra = use_non_coadded_spectra
 
-    Methods
-    -------
-    filter_forests (from Data)
-    set_blinding (from Data)
-    __init__
-    read_data
-    read_file
-
-    Attributes
-    ----------
-    analysis_type: str (from Data)
-    Selected analysis type. Current options are "BAO 3D" or "PK 1D"
-
-    forests: list of Forest (from Data)
-    A list of Forest from which to compute the deltas.
-
-    min_num_pix: int (from Data)
-    Minimum number of pixels in a forest. Forests with less pixels will be dropped.
-
-    blinding: str (from DesiData)
-    A string specifying the chosen blinding strategies. Must be one of the
-    accepted values in ACCEPTED_BLINDING_STRATEGIES
-
-    catalogue: astropy.table.Table (from DesiData)
-    The quasar catalogue
-
-    input_directory: str (from DesiData)
-    Directory to spectra files.
-
-    in_nside: 64 or 16
-    Nside used in the folder structure (64 for data and 16 for mocks)
-
-    logger: logging.Logger
-    Logger object
-    """
-    def __init__(self, config):
-        """Initialize class instance
-
-        Arguments
-        ---------
-        config: configparser.SectionProxy
-        Parsed options to initialize class
-        """
-        self.logger = logging.getLogger(__name__)
-
-        self.use_non_coadded_spectra = None
-        self.__parse_config(config)
-        #init of DesiData needs to come last, as it contains the actual data reading and thus needs all config
-        super().__init__(config)
-
-
-
-    def __parse_config(self, config):
-        """Parse the configuration options
-
-        Arguments
-        ---------
-        config: configparser.SectionProxy
-        Parsed options to initialize class
-
-        Raise
-        -----
-        DataError upon missing required variables
-        """
-        self.use_non_coadded_spectra = config.getboolean(
-            "use non-coadded spectra")
-        if self.use_non_coadded_spectra is None:
-            raise DataError(
-                "Missing argument 'use non-coadded spectra' required by DesiHealpix"
-            )
-        self.num_processors = config.getint("num processors")
-        if self.num_processors is None:
-            raise DataError(
-                "Missing argument 'num processors' required by DesiHealpix")
-        if self.num_processors == 0:
-            self.num_processors = (multiprocessing.cpu_count() // 2)
-
-    def read_data(self):
-        """Read the data.
-
-        Method used to read healpix-based survey data.
-
-        Return
-        ------
-        is_mock: bool
-        False as DESI data are not mocks
-
-        is_sv: bool
-        True if all the read data belong to SV. False otherwise
-
-        Raise
-        -----
-        DataError if no quasars were found
-        """
-        in_nside = 64
-
-        healpix = [
-            healpy.ang2pix(in_nside,
-                           np.pi / 2 - row["DEC"],
-                           row["RA"],
-                           nest=True)
-            for row in self.catalogue
-        ]
-        self.catalogue["HEALPIX"] = healpix
-        self.catalogue.sort("HEALPIX")
-        grouped_catalogue = self.catalogue.group_by(["HEALPIX", "SURVEY"])
-
-        is_sv = True
-        if self.num_processors>1:
-            context = multiprocessing.get_context('fork')
-            manager =  multiprocessing.Manager()
-            forests_by_targetid = manager.dict()
-            arguments = []
-            for (index,
-                (healpix, survey)), group in zip(enumerate(grouped_catalogue.groups.keys),
-                                        grouped_catalogue.groups):
-
-                if survey not in ["sv", "sv1", "sv2", "sv3"]:
-                    is_sv = False
-
-                input_directory = f'{self.input_directory}/{survey}/dark'
-                coadd_name = "spectra" if self.use_non_coadded_spectra else "coadd"
-                filename = (
-                    f"{input_directory}/{healpix//100}/{healpix}/{coadd_name}-{survey}-"
-                    f"dark-{healpix}.fits")
-
-                arguments.append((filename,group,forests_by_targetid))
-
-                self.logger.info(f"reading data from {len(arguments)} files")
-            with context.Pool(processes=self.num_processors) as pool:
-
-                pool.starmap(self.read_file, arguments)
-        else:
-            forests_by_targetid = {}
-            for (index,
-                (healpix,
-                survey)), group in zip(enumerate(grouped_catalogue.groups.keys),
-                                        grouped_catalogue.groups):
-
-                if survey not in ["sv", "sv1", "sv2", "sv3"]:
-                    is_sv = False
-
-                input_directory = f'{self.input_directory}/{survey}/dark'
-                coadd_name = "spectra" if self.use_non_coadded_spectra else "coadd"
-                filename = (
-                    f"{input_directory}/{healpix//100}/{healpix}/{coadd_name}-{survey}-"
-                    f"dark-{healpix}.fits")
-
-                #TODO: not sure if we want the dark survey to be hard coded in here, probably won't run on anything else, but still
-
-                self.logger.progress(
-                    f"Read {index} of {len(grouped_catalogue.groups.keys)}. "
-                    f"num_data: {len(forests_by_targetid)}")
-
-                self.read_file(filename, group, forests_by_targetid)
-
-        if len(forests_by_targetid) == 0:
-            raise DataError("No Quasars found, stopping here")
-
-        self.forests = list(forests_by_targetid.values())
-
-        return False, is_sv
-
-    def read_file(self, filename, catalogue, forests_by_targetid):
+    def read_file(self, filename, catalogue):
         """Read the spectra and formats its data as Forest instances.
 
         Arguments
@@ -202,6 +43,8 @@ class DesiHealpix(DesiData):
         catalogue: astropy.table.Table
         The quasar catalogue fragment associated with this file
 
+        Returns:
+        ---------
         forests_by_targetid: dict
         Dictionary were forests are stored. Its content is modified by this
         function with the new forests.
@@ -221,6 +64,7 @@ class DesiHealpix(DesiData):
         # First read all wavelength, flux, ivar, mask, and resolution
         # from this file
         spectrographs_data = {}
+        forests_by_targetid = {}
         colors = ["B", "R"]
         if "Z_FLUX" in hdul:
             colors.append("Z")
@@ -351,3 +195,205 @@ class DesiHealpix(DesiData):
                     forests_by_targetid[targetid] = existing_forest
                 else:
                     forests_by_targetid[targetid] = forest
+
+        return forests_by_targetid
+
+    def __call__(self, X):
+        filename, catalogue = X
+
+        return self.read_file(filename, catalogue)
+
+
+class DesiHealpix(DesiData):
+    """Reads the spectra from DESI using healpix mode and formats its data as a
+    list of Forest instances.
+
+    Methods
+    -------
+    filter_forests (from Data)
+    set_blinding (from Data)
+    __init__
+    read_data
+    read_file
+
+    Attributes
+    ----------
+    analysis_type: str (from Data)
+    Selected analysis type. Current options are "BAO 3D" or "PK 1D"
+
+    forests: list of Forest (from Data)
+    A list of Forest from which to compute the deltas.
+
+    min_num_pix: int (from Data)
+    Minimum number of pixels in a forest. Forests with less pixels will be dropped.
+
+    blinding: str (from DesiData)
+    A string specifying the chosen blinding strategies. Must be one of the
+    accepted values in ACCEPTED_BLINDING_STRATEGIES
+
+    catalogue: astropy.table.Table (from DesiData)
+    The quasar catalogue
+
+    input_directory: str (from DesiData)
+    Directory to spectra files.
+
+    in_nside: 64 or 16
+    Nside used in the folder structure (64 for data and 16 for mocks)
+
+    logger: logging.Logger
+    Logger object
+    """
+    def __init__(self, config):
+        """Initialize class instance
+
+        Arguments
+        ---------
+        config: configparser.SectionProxy
+        Parsed options to initialize class
+        """
+        self.logger = logging.getLogger(__name__)
+
+        self.use_non_coadded_spectra = None
+        self.__parse_config(config)
+        #init of DesiData needs to come last, as it contains the actual data reading and thus needs all config
+        super().__init__(config)
+
+
+
+    def __parse_config(self, config):
+        """Parse the configuration options
+
+        Arguments
+        ---------
+        config: configparser.SectionProxy
+        Parsed options to initialize class
+
+        Raise
+        -----
+        DataError upon missing required variables
+        """
+        self.use_non_coadded_spectra = config.getboolean(
+            "use non-coadded spectra")
+        if self.use_non_coadded_spectra is None:
+            raise DataError(
+                "Missing argument 'use non-coadded spectra' required by DesiHealpix"
+            )
+        self.num_processors = config.getint("num processors")
+        if self.num_processors is None:
+            raise DataError(
+                "Missing argument 'num processors' required by DesiHealpix")
+        if self.num_processors == 0:
+            self.num_processors = (multiprocessing.cpu_count() // 2)
+
+    def read_data(self):
+        """Read the data.
+
+        Method used to read healpix-based survey data.
+
+        Return
+        ------
+        is_mock: bool
+        False as DESI data are not mocks
+
+        is_sv: bool
+        True if all the read data belong to SV. False otherwise
+
+        Raise
+        -----
+        DataError if no quasars were found
+        """
+        in_nside = 64
+
+        healpix = [
+            healpy.ang2pix(in_nside,
+                           np.pi / 2 - row["DEC"],
+                           row["RA"],
+                           nest=True)
+            for row in self.catalogue
+        ]
+        self.catalogue["HEALPIX"] = healpix
+        self.catalogue.sort("HEALPIX")
+        grouped_catalogue = self.catalogue.group_by(["HEALPIX", "SURVEY"])
+
+        if self.analysis_type == "PK 1D":
+            if "exposures_diff" not in Forest.mask_fields:
+                Forest.mask_fields += ["exposures_diff"]
+            if "reso" not in Forest.mask_fields:
+                Forest.mask_fields += ["reso"]
+            if "reso_pix" not in Forest.mask_fields:
+                Forest.mask_fields += ["reso_pix"]
+            if "resolution_matrix" not in Forest.mask_fields:
+                Forest.mask_fields += ["resolution_matrix"]
+
+        is_sv = True
+        forests_by_targetid = {}
+        
+        def _merge_new_forest(forests_by_pe):
+            parent_targetids = set(forests_by_targetid.keys())
+            existing_targetids = parent_targetids.intersection(forests_by_pe.keys())
+            new_targetids = forests_by_pe.keys()-existing_targetids
+
+            # Does not fail if existing_targetids is empty
+            for tid in existing_targetids:
+                existing_forest = forests_by_targetid[tid]
+                existing_forest.coadd(forests_by_pe[tid])
+                forests_by_targetid[tid] = existing_forest
+            for tid in new_targetids:
+                forests_by_targetid[tid] = forests_by_pe[tid]
+
+        if self.num_processors>1:
+            # Set up arguments to pass to each PE
+            arguments = []
+            for (index,
+                (healpix, survey)), group in zip(enumerate(grouped_catalogue.groups.keys),
+                                        grouped_catalogue.groups):
+
+                if survey not in ["sv", "sv1", "sv2", "sv3"]:
+                    is_sv = False
+
+                input_directory = f'{self.input_directory}/{survey}/dark'
+                coadd_name = "spectra" if self.use_non_coadded_spectra else "coadd"
+                filename = (
+                    f"{input_directory}/{healpix//100}/{healpix}/{coadd_name}-{survey}-"
+                    f"dark-{healpix}.fits")
+
+                arguments.append((filename,group))
+
+                self.logger.info(f"reading data from {len(arguments)} files")
+
+            with multiprocessing.Pool(processes=self.num_processors) as pool:
+                imap_it = pool.imap(ParallelReader(self.analysis_type, self.use_non_coadded_spectra, self.logger), arguments)
+                for forests_by_pe in imap_it:
+                    # Merge each dict to master forests_by_targetid
+                    _merge_new_forest(forests_by_pe)
+                    
+        else:
+            reader = ParallelReader(self.analysis_type, self.use_non_coadded_spectra, self.logger)
+            for (index,
+                (healpix,
+                survey)), group in zip(enumerate(grouped_catalogue.groups.keys),
+                                        grouped_catalogue.groups):
+
+                if survey not in ["sv", "sv1", "sv2", "sv3"]:
+                    is_sv = False
+
+                input_directory = f'{self.input_directory}/{survey}/dark'
+                coadd_name = "spectra" if self.use_non_coadded_spectra else "coadd"
+                filename = (
+                    f"{input_directory}/{healpix//100}/{healpix}/{coadd_name}-{survey}-"
+                    f"dark-{healpix}.fits")
+
+                #TODO: not sure if we want the dark survey to be hard coded in here, probably won't run on anything else, but still
+
+                self.logger.progress(
+                    f"Read {index} of {len(grouped_catalogue.groups.keys)}. "
+                    f"num_data: {len(forests_by_targetid)}")
+
+                _merge_new_forest(reader(filename, group))
+
+        if len(forests_by_targetid) == 0:
+            raise DataError("No Quasars found, stopping here")
+
+        self.forests = list(forests_by_targetid.values())
+
+        return False, is_sv
