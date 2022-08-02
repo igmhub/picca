6465bd682165ab861e9d30058f2498b3a1828fc9
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2022-08-02 17:02:04 -0400

    fix import from picca.delta_extraction.least_squares.least_squares_var_stats import (     LeastsSquaresVarStats

--- py/picca/delta_extraction/expected_fluxes/true_continuum.py
+++ py/picca/delta_extraction/expected_fluxes/true_continuum.py
@@ -12,6 +12,7 @@ from picca.delta_extraction.astronomical_objects.forest import Forest
 from picca.delta_extraction.astronomical_objects.pk1d_forest import Pk1dForest
 from picca.delta_extraction.errors import ExpectedFluxError
 from picca.delta_extraction.expected_flux import ExpectedFlux, defaults, accepted_options
+from picca.delta_extraction.least_squares.least_squares_var_stats import LeastsSquaresVarStats
 from picca.delta_extraction.utils import (find_bins, update_accepted_options,
                                           update_default_options)
 
