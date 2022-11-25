1b7f08cbb0a27e64bc2e3f90165623a849b90924
corentinravoux <corentin.ravoux.research@gmail.com>
2022-11-25 10:28:45 +0100

    end of mean pk1d launcher

--- bin/picca_Pk1D_postprocess.py
+++ bin/picca_Pk1D_postprocess.py
@@ -9,29 +9,30 @@ from picca.pk1d import postproc_pk1d
 
 
 
+def define_wavevector_limits(k_inf,k_sup,k_dist,velunits,pixsize,rebinfac):
+    """ Define the wavenumber vector limits and binning
+        Default binning define linearly with assumed average redshift of 3.4,
+        and a forest defined between 1050 and 1200 Angstrom.
+        Default velocity binning with same number of pixel"""
 
-def define_wavevector_limits(args,velunits,pixsize):
+    k_inf_lin_default = 2*np.pi/((1200-1050)*(1+3.4)/rebinfac)
+    k_sup_lin_default = np.pi/pixsize
+    nb_k_bin_lin = int(k_sup_lin_default/k_inf_lin_default/4)
+    k_dist_lin_default = (k_sup_lin_default-k_inf_lin_default) / nb_k_bin_lin
+
+    k_inf_vel_default = 0.000813
+    k_dist_vel_default = 0.000542 * rebinfac
+    k_sup_vel_default = k_inf_vel_default + nb_k_bin_lin * k_dist_vel_default
+
     if velunits:
-            k_inf=k_inf_vel
-            k_sup=k_sup_vel
-            k_dist=k_bin_dist_vel
-        else:
-            k_inf=0.000813
-            k_dist=0.000542*args["rebinfac"]
-            k_inf_lin=2*np.pi/((1200-1050)*(1+3.4)/args["rebinfac"])
-            k_sup_lin=np.pi/pixsize
-            nb_k_bin=int(k_sup_lin/k_inf_lin/4)
-            k_sup=k_inf + nb_k_bin*k_dist
+        if k_inf is None: k_inf = k_inf_vel_default
+        if k_sup is None: k_sup = k_sup_vel_default
+        if k_dist is None: k_dist = k_dist_vel_default
     else:
-        if("k_inf_lin" in args.keys()):
-            k_inf=args["k_inf_lin"]
-            k_sup=args["k_sup_lin"]
-            k_dist=args["k_bin_dist_lin"]
-        else:
-            k_inf=2*np.pi/((1200-1050)*(1+3.4)/args["rebinfac"])
-            k_sup=np.pi/pixsize
-            nb_k_bin=int(k_sup/k_inf/4)
-            k_dist=(k_sup-k_inf)/nb_k_bin
+        if k_inf is None: k_inf = k_inf_lin_default
+        if k_sup is None: k_sup = k_sup_lin_default
+        if k_dist is None: k_dist = k_dist_lin_default
+
     return(k_inf,k_sup,k_dist)
 
 
@@ -54,19 +55,60 @@ def main(cmdargs):
                         type=float,
                         default='2.1',
                         required=False,
-                        help='')
+                        help='Minimal value of the redshift edge array,'
+                             'Default value: 2.1')
 
     parser.add_argument('--zedge-max',
                         type=float,
                         default='6.3',
                         required=False,
-                        help='')
+                        help='Maximal value of the redshift edge array,'
+                             'Default value: 6.3')
 
-    parser.add_argument('--zbin-size',
+    parser.add_argument('--zedge-bin',
                         type=float,
                         default='0.2',
                         required=False,
-                        help='')
+                        help='Number of bins of the redshift edge array,'
+                             'Default value: 6.3')
+
+    parser.add_argument('--kedge-min',
+                        type=float,
+                        default=None,
+                        required=False,
+                        help='Minimal value of the wavenumber edge array,'
+                             'Default value defined in function of --rebinfac, '
+                             '--pixsize, and --velunits arguments')
+
+    parser.add_argument('--kedge-max',
+                        type=float,
+                        default=None,
+                        required=False,
+                        help='Maximal value of the wavenumber edge array,'
+                             'Default value defined in function of --rebinfac, '
+                             '--pixsize, and --velunits arguments')
+
+    parser.add_argument('--kedge-bin',
+                        type=float,
+                        default=None,
+                        required=False,
+                        help='Number of bins of the wavenumber edge array,'
+                             'Default value defined in function of --rebinfac, '
+                             '--pixsize, and --velunits arguments')
+
+    parser.add_argument('--rebinfac',
+                        type=int,
+                        default=1,
+                        required=False,
+                        help='Rebining factor used to define the binning of '
+                             'the output wavenumber array')
+
+    parser.add_argument('--pixsize',
+                        type=float,
+                        default=0.8,
+                        required=False,
+                        help='Size of a spectrum pixel in Angstrom, used to'
+                             'define the binning of the output wavenumber array')
 
     parser.add_argument('--weights-method',
                         type=str,
@@ -79,19 +121,20 @@ def main(cmdargs):
                         action='store_true',
                         default=True,
                         required=False,
-                        help='')
+                        help='Apply a redshift-dependent SNR quality cut')
 
     parser.add_argument('--snr-cut-scheme',
                         type=str,
                         default='eboss',
                         required=False,
-                        help='')
+                        help='Choice of the type of SNR cut, '
+                             'Possible options: eboss')
 
     parser.add_argument('--overwrite',
                         action='store_true',
                         default=True,
                         required=False,
-                        help='')
+                        help='Overwrite the output')
 
     parser.add_argument('--velunits',
                         action='store_true',
@@ -103,62 +146,54 @@ def main(cmdargs):
                         action='store_true',
                         default=True,
                         required=False,
-                        help='')
-
-
-
+                        help='Used averages instead of medians')
 
 
     args = parser.parse_args(sys.argv[1:])
 
+    if (args.weights_method != "no_weights") & (args.apply_mean_snr_cut):
+        raise ValueError("""You are using a weighting method with a
+                            redshift-dependent SNR quality cut, this is not
+                            tested and should bias the result""")
+
     if args.apply_mean_snr_cut:
         if args.snr_cut_scheme is "eboss":
-            snr_cut_mean = [4.1, 3.9, 3.6, 3.2, 2.9, 2.6, 2.2, 2.0, 2.0, 2.0, 2.0,
-                            2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0]
-            zbins =        [2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2,
-                            4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6, 5.8, 6.0, 6.2]
+            snr_cut_mean =       [4.1, 3.9, 3.6, 3.2, 2.9, 2.6, 2.2, 2.0, 2.0,
+                                  2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0,
+                                  2.0, 2.0, 2.0]
+            zbins_snr_cut_mean = [2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8,
+                                  4.0, 4.2, 4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6,
+                                  5.8, 6.0, 6.2]
         else:
             raise ValueError("Please choose the snr cutting scheme to be eboss, "
                              "or turn of the --apply-mean-snr-cut parameter, or "
                              "add here in the code a specific snr cutting scheme")
     else:
         snr_cut_mean = None
-        zbins = None
-
-    zbin_edges = np.arange(args.zedge_min, args.zedge_max, args.zbin_size)
+        zbins_snr_cut_mean = None
 
 
-    #
-    # # kbin_edges computation
-    # rebinfac=3
-    # pixsize=0.8
-    # k_inf_lin=2*np.pi/((1200-1050)*(1+3.4)/rebinfac)
-    # k_sup_lin=np.pi/pixsize
-    # nb_k_bin=int(k_sup_lin/k_inf_lin/4)
-    # k_bin_dist_lin=(k_sup_lin-k_inf_lin)/nb_k_bin
-    # k_inf_vel=0.000813
-    # k_bin_dist_vel=0.000542*rebinfac
-    # k_sup_vel=k_inf_vel + nb_k_bin*k_bin_dist_vel
-    # args={}
-    # args['z_binsize']=0.2
-    # args['k_inf_lin']=k_inf_lin
-    # args['k_sup_lin']=k_sup_lin
-    # args['k_bin_dist_lin']=k_bin_dist_lin
-    # args['k_inf_vel']=k_inf_vel
-    # args['k_sup_vel']=k_sup_vel
-    # args['k_bin_dist_vel']=k_bin_dist_vel
-    # k_inf, k_sup, k_dist = define_wavevector_limits(args,velunits,pixsize)
-    # kbin_edges=np.arange(k_inf,k_sup,k_dist)
-    # print(kbin_edges)
 
+
+
+    kedge_min, kedge_max, kedge_bin = define_wavevector_limits(args.kedge_min,
+                                                               args.kedge_max,
+                                                               args.kedge_bin,
+                                                               args.velunits,
+                                                               args.pixsize,
+                                                               args.rebinfac)
+
+
+    k_edges = np.arange(kedge_min,kedge_max,kedge_bin)
+    z_edges = np.arange(args.zedge_min, args.zedge_max, args.zbin_size)
 
 
     data = postproc_pk1d.parallelize_p1d_comp(args.in_dir,
-                                              zbin_edges,
-                                              kbin_edges,
+                                              z_edges,
+                                              k_edges,
                                               weights_method=args.weights_method,
                                               snr_cut_mean=snr_cut_mean,
-                                              zbins=zbins,
+                                              zbins=zbins_snr_cut_mean,
                                               nomedians=args.no_medians,
                                               velunits=args.velunits,
                                               overwrite=args.overwrite)
