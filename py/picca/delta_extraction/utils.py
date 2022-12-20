685bf80664bf54ed839830bacb034da083941ffb
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2022-12-20 14:09:51 +0100

    Log messages should now be outputted to stdout

--- py/picca/delta_extraction/utils.py
+++ py/picca/delta_extraction/utils.py
@@ -248,7 +248,7 @@ def setup_logger(logging_level_console=logging.DEBUG,
     formatter = logging.Formatter('[%(levelname)s]: %(message)s')
 
     # create console handler to logs messages
-    console_handler = logging.StreamHandler()
+    console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setLevel(logging_level_console)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
