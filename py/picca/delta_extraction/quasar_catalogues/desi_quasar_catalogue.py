a6c6937bb599cfcad3d59ab4d98ca382efc2b815
alxogm <alxogm@github.com>
2023-02-01 12:07:57 -0800

    Change LASTNIGHT for COADD_LASTNIGHT

--- py/picca/delta_extraction/quasar_catalogues/desi_quasar_catalogue.py
+++ py/picca/delta_extraction/quasar_catalogues/desi_quasar_catalogue.py
@@ -167,9 +167,8 @@ class DesiQuasarCatalogue(QuasarCatalogue):
                     "expected to be present but it is not.")
         if 'NIGHT' in catalogue.colnames:
             keep_columns += ['NIGHT']
-        # TODO: remove this once we settle on a name for LAST_NIGHT/LASTNIGHT
-        if "LAST_NIGHT" in catalogue.colnames:
-            catalogue.rename_column("LAST_NIGHT", "LASTNIGHT")
+        if "COADD_LASTNIGHT" in catalogue.colnames:
+            catalogue.rename_column("COADD_LASTNIGHT", "LASTNIGHT")
         if 'LASTNIGHT' in catalogue.colnames:
             keep_columns += ['LASTNIGHT']
         if 'SURVEY' in catalogue.colnames:
