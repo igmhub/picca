ab19fe2a5b9932ccab3999af9dd76a4ad0cd3e79
Michael Walther <Waelthus@users.noreply.github.com>
2023-08-03 11:20:44 +0200

    round print statemen

--- bin/picca_cf1d.py
+++ bin/picca_cf1d.py
@@ -229,13 +229,13 @@ def main(cmdargs):
         if args.in_dir2:
             log_lambda_min2=np.min([d.log_lambda[0] for hp in cf.data2.values() for d in hp])
             cf.log_lambda_min=np.min([cf.log_lambda_min,log_lambda_min2])
-        print(f"lambda_min={10**cf.log_lambda_min}")
+        print(f"lambda_min={10**cf.log_lambda_min:.3f}")
     if args.lambda_max is None:
         cf.log_lambda_max=np.max([d.log_lambda[-1] for hp in cf.data.values() for d in hp])
         if args.in_dir2:
             log_lambda_max2=np.max([d.log_lambda[-1] for hp in cf.data2.values() for d in hp])
             cf.log_lambda_max=np.max([cf.log_lambda_max,log_lambda_max2])
-        print(f"lambda_max={10**cf.log_lambda_max}")
+        print(f"lambda_max={10**cf.log_lambda_max:.3f}")
     
 
     cf.num_pixels = int((cf.log_lambda_max - cf.log_lambda_min) /
