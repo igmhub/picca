p1d: fixes and updates for skyline masking correction

py/picca/pk1d/compute_pk1d.py
@@ -168,8 +168,8 @@ def split_forest_in_z_parts(z_grid,
 
     for loglam_min_chunk, loglam_max_chunk in zip(loglam_min_grid, loglam_max_grid):
         selection = (
-            (delta.log_lambda >= loglam_min_chunk) &
-            (delta.log_lambda < loglam_max_chunk))
+            (log_lambda >= loglam_min_chunk) &
+            (log_lambda < loglam_max_chunk))
         if min_num_pixels is not None and np.sum(selection) < min_num_pixels:
             continue
         if np.sum(log_lambda <= loglam_min_chunk)==0 or np.sum(log_lambda >= loglam_max_chunk)==0:

py/picca/pk1d/postproc_pk1d.py
@@ -98,16 +98,19 @@ def read_pk1d(filename, kbin_edges, snrcut=None, zbins_snrcut=None, skymask_matr
             if skymask_matrices is not None:
                 chunk_table["Pk_raw_skycorr"] = chunk_table["Pk_raw"]
                 chunk_table["Pk_noise_skycorr"] = chunk_table["Pk_noise"]
-                w, = np.where(np.isclose(meanz_skymatrices, chunk_header["MEANZ"], atol=1.e-3))
-                if len(w)!=1:
-                    userprint(f"file {filename} hdu {i+1}: MEANZ does not match skymatrices.")
-                else:
+                w, = np.where(np.isclose(meanz_skymatrices, chunk_header["MEANZ"], atol=1.e-2))
+                if len(w)==1:
                     correction_matrix = skymask_matrices[w[0]][1]
+                    ll = len(chunk_table["Pk_raw"])
+                    correction_matrix = np.copy(correction_matrix[0:ll, 0:ll])
                     if len(chunk_table["Pk_raw"])!=correction_matrix.shape[0]:
                         userprint(f"file {filename} hdu {i+1}: Pk_raw doesnt match shape of skymatrix.")
                     else:
                         chunk_table["Pk_raw_skycorr"] = correction_matrix @ chunk_table["Pk_raw"]
                         chunk_table["Pk_noise_skycorr"] = correction_matrix @ chunk_table["Pk_noise"]
+                chunk_table["Pk_skycorr"] = (
+                        chunk_table["Pk_raw_skycorr"] - chunk_table["Pk_noise_skycorr"]
+                    ) / chunk_table["cor_reso"]
 
             chunk_table["forest_z"] = float(chunk_header["MEANZ"])
             chunk_table["forest_snr"] = float(chunk_header["MEANSNR"])
@@ -151,6 +154,8 @@ def read_pk1d(filename, kbin_edges, snrcut=None, zbins_snrcut=None, skymask_matr
     p1d_table["Pk_norescor"] = p1d_table["Pk_raw"] - p1d_table["Pk_noise"]
     p1d_table["Pk_nonoise"] = p1d_table["Pk_raw"] / p1d_table["cor_reso"]
     p1d_table["Pk_noraw"] = p1d_table["Pk_noise"] / p1d_table["cor_reso"]
+    if skymask_matrices is not None:
+        p1d_table["Delta2_skycorr"] = p1d_table["k"] * p1d_table["Pk_skycorr"] / np.pi
     try:
         p1d_table["Pk_noraw_miss"] = p1d_table["Pk_noise_miss"] / p1d_table["cor_reso"]
     except KeyError:

py/picca/pk1d/utils.py
@@ -17,12 +17,11 @@ DEFAULT_K_BINNING_FACTOR = 4
 # used in mean Pk computation when weight_method=='fit_snr':
 MEANPK_FITRANGE_SNR = [1, 10]
 
-# DESI wavelength grid, used to estimate skyline masks
-#   this is the "standard" wavegrid, see eg. desispec/scripts/proc.py
-#   (arguments of desi_extract_spectra)
-MINWAVE_DESI = 3600.0
-MAXWAVE_DESI = 9824.0
-DWAVE_DESI = 0.8
+# Wavelength grid to estimate skyline masks:
+# default values from eg. desispec/scripts/proc.py
+DEFAULT_MINWAVE_SKYMASK = 3600.0
+DEFAULT_MAXWAVE_SKYMASK = 9824.0
+DEFAULT_DWAVE_SKYMASK = 0.8
 
 
 def fitfunc_variance_pk1d(snr, amp, zero_point):
@@ -46,7 +45,10 @@ def fitfunc_variance_pk1d(snr, amp, zero_point):
     return (amp / (snr - 1)**2) + zero_point
 
 
-def skyline_mask_matrices_desi(z_parts, skyline_mask_file):
+def skyline_mask_matrices_desi(z_parts, skyline_mask_file,
+                               minwave=DEFAULT_DWAVE_SKYMASK,
+                               maxwave=DEFAULT_MAXWAVE_SKYMASK,
+                               dwave=DEFAULT_DWAVE_SKYMASK):
     """ Compute matrices to correct for the masking effect on FFTs,
     when chunks are defined with the `--parts-in-redshift` option in picca_Pk1D.
     Only implemented for DESI data, ie delta lambda = 0.8 and no rebinning in the deltas.
@@ -59,6 +61,10 @@ def skyline_mask_matrices_desi(z_parts, skyline_mask_file):
     skyline_mask_file: str
     Name of file containing the list of skyline masks
 
+    minwave, maxwave, dwave: float
+    Parameter defining the wavelength grid used for the skymask. Should be consistent with
+    arguments lambda min, lambda max, delta lambda used in picca_delta_extraction.py.
+
     Return
     ------
     A list with N items [meanz, matrix] where N = len(z_parts)-1 is the number of redshift chunks.
@@ -68,19 +74,19 @@ def skyline_mask_matrices_desi(z_parts, skyline_mask_file):
     """
     skyline_list = np.genfromtxt(skyline_mask_file,
                                  names=('type', 'wave_min', 'wave_max', 'frame'))
-    desi_wavegrid = np.arange(MINWAVE_DESI, MAXWAVE_DESI, DWAVE_DESI)
+    ref_wavegrid = np.arange(minwave, maxwave, dwave)
     num_parts = len(z_parts)-1
-    out = {}
+    out = []
 
     for iz in range(num_parts):
-        lmin = constants.ABSORBER_IGM['LYA'] * (1+zgrid[iz])
-        lmax = constants.ABSORBER_IGM['LYA'] * (1+zgrid[iz+1])
+        lmin = constants.ABSORBER_IGM['LYA'] * (1+z_parts[iz])
+        lmax = constants.ABSORBER_IGM['LYA'] * (1+z_parts[iz+1])
         #- the following selection is identical to compute_pk1d.split_forest_in_z_parts:
-        wave = desi_wavegrid[ (desi_wavegrid>=lmin) & (desi_wavegrid<lmax) ]
+        wave = ref_wavegrid[ (ref_wavegrid>=lmin) & (ref_wavegrid<lmax) ]
         npts = len(wave)
         skymask = np.ones(npts)
-        selection = ( (skymask_list['wave_min']<=lmax) & (skymask_list['wave_min']>=lmin)
-                    (skymask_list['wave_max']<=lmax) & (skymask_list['wave_max']>=lmin) )
+        selection = ( (skyline_list['wave_min']<=lmax) & (skyline_list['wave_min']>=lmin)
+                    ) | ( (skyline_list['wave_max']<=lmax) & (skyline_list['wave_max']>=lmin) )
         for skyline in skyline_list[selection]:
             skymask[(wave>=skyline['wave_min']) & (wave<=skyline['wave_max'])] = 0
         skymask_tilde = np.fft.fft(skymask)/npts
@@ -91,11 +97,11 @@ def skyline_mask_matrices_desi(z_parts, skyline_mask_file):
                 mask_matrix[j,l] = skymask_tilde[index_mask].real**2 + skymask_tilde[index_mask].imag**2
         try:
             inv_matrix = np.linalg.inv(mask_matrix)
-        except LinAlgError:
-            userprint(f"Warning: cannot invert sky mask matrix for z bin {zgrid[iz]} - {zgrid[iz+1]}")
+        except np.linalg.LinAlgError:
+            userprint(f"Warning: cannot invert sky mask matrix for z bin {z_parts[iz]} - {z_parts[iz+1]}")
             userprint("No correction will be applied for this bin")
             inv_matrix = np.eye(npts)
-        meanz = (zgrid[iz]+zgrid[iz+1])/2
+        meanz = (z_parts[iz]+z_parts[iz+1])/2
         out.append([meanz, inv_matrix])
 
     return out