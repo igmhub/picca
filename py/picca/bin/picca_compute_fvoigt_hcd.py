dc192822a8cc3fe52d911d63a4119fe3845cad26
corentinravoux <corentin.ravoux.research@gmail.com>
2025-05-21 18:01:54 +0200

    moving the script to the new location

--- py/picca/bin/picca_compute_fvoigt_hcd.py
+++ py/picca/bin/picca_compute_fvoigt_hcd.py
@@ -7,7 +7,11 @@ from scipy.special import wofz
 
 from picca import constants
 from picca.delta_extraction.masks.dla_mask import (
-    dla_profile, LAMBDA_LYA, OSCILLATOR_STRENGTH_LYA, GAMMA_LYA)
+    GAMMA_LYA,
+    LAMBDA_LYA,
+    OSCILLATOR_STRENGTH_LYA,
+    dla_profile,
+)
 
 
 def profile_wave_to_comov_dist(wave, profile_wave, cosmo, differential=False):
@@ -37,9 +41,13 @@ def profile_wave_to_comov_dist(wave, profile_wave, cosmo, differential=False):
     if differential:
         # We are in the f(lambda)dlambda = f(r)dr case,
         # so need to account for dlambda / dr = H(z) * Lambda_Lya / c
-        profile_comov_dist = cosmo.get_hubble(z) * constants.ABSORBER_IGM["LYA"] / speed_of_light
+        profile_comov_dist = (
+            cosmo.get_hubble(z) * constants.ABSORBER_IGM["LYA"] / speed_of_light
+        )
 
-    profile_comov_dist = np.interp(lin_spaced_comov_dist, comov_dist, profile_comov_dist)
+    profile_comov_dist = np.interp(
+        lin_spaced_comov_dist, comov_dist, profile_comov_dist
+    )
 
     return lin_spaced_comov_dist, profile_comov_dist
 
@@ -68,30 +76,60 @@ def fft_profile(profile, dx):
 
 
 def main():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-                                     description=('Compute FVoigt profile'))
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=("Compute FVoigt profile"),
+    )
 
-    parser.add_argument('-o', '--output', type=str, required=True,
-                        help=('Name and path of file to write FVoigt profile to'))
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        required=True,
+        help=("Name and path of file to write FVoigt profile to"),
+    )
 
-    parser.add_argument('--weights', type=str, required=True,
-                        help=('File with total weight as a function of observed wavelength'))
+    parser.add_argument(
+        "--weights",
+        type=str,
+        required=True,
+        help=("File with total weight as a function of observed wavelength"),
+    )
 
-    parser.add_argument('--cddf', type=str, required=True,
-                        help=('File with column density distribution function (CDDF)'))
+    parser.add_argument(
+        "--cddf",
+        type=str,
+        required=True,
+        help=("File with column density distribution function (CDDF)"),
+    )
 
-    parser.add_argument('--dla-prob', type=str, required=True,
-                        help=('File with probability of finding DLAs in a forest '
-                              'as a function of observed wavelength'))
+    parser.add_argument(
+        "--dla-prob",
+        type=str,
+        required=True,
+        help=(
+            "File with probability of finding DLAs in a forest "
+            "as a function of observed wavelength"
+        ),
+    )
 
-    parser.add_argument('--z-dla', type=float, required=True,
-                        help=('Mean DLA redshift'))
+    parser.add_argument(
+        "--z-dla", type=float, required=True, help=("Mean DLA redshift")
+    )
 
-    parser.add_argument('--normalize', action='store_true', required=False,
-                        help=('Whether the output FVoigt function is normalized'))
+    parser.add_argument(
+        "--normalize",
+        action="store_true",
+        required=False,
+        help=("Whether the output FVoigt function is normalized"),
+    )
 
-    parser.add_argument('--positive-fvoigt', action='store_true', required=False,
-                        help=('Whether the output FVoigt function should be positive'))
+    parser.add_argument(
+        "--positive-fvoigt",
+        action="store_true",
+        required=False,
+        help=("Whether the output FVoigt function should be positive"),
+    )
 
     args = parser.parse_args()
 
@@ -102,12 +140,15 @@ def main():
     fidcosmo = constants.Cosmo(Om=0.3147)
 
     comov_dist_prob, dla_prob_comov_dist = profile_wave_to_comov_dist(
-        dla_prob_wave[0], dla_prob_wave[1], fidcosmo, differential=True)
+        dla_prob_wave[0], dla_prob_wave[1], fidcosmo, differential=True
+    )
     comov_dist_weights, weights_comov_dist = profile_wave_to_comov_dist(
-        weights_wave[:, 0], weights_wave[:, 1], fidcosmo)
+        weights_wave[:, 0], weights_wave[:, 1], fidcosmo
+    )
 
     weight_interp = np.interp(
-        comov_dist_prob, comov_dist_weights, weights_comov_dist, left=0, right=0)
+        comov_dist_prob, comov_dist_weights, weights_comov_dist, left=0, right=0
+    )
     mean_density = np.average(dla_prob_comov_dist, weights=weight_interp)
 
     wave = np.arange(2000, 8000, 1)  # TODO this grid may be too sparse
@@ -117,8 +158,12 @@ def main():
         profile_wave /= np.mean(profile_wave)
 
         # r is in Mpc h^-1 --> k (from tf) will be in (Mpc h^-1)^-1 = h Mpc^-1 :)
-        comov_dist, profile_comov_dist = profile_wave_to_comov_dist(wave, profile_wave, fidcosmo)
-        k, fourier_profile = fft_profile(profile_comov_dist, np.abs(comov_dist[1] - comov_dist[0]))
+        comov_dist, profile_comov_dist = profile_wave_to_comov_dist(
+            wave, profile_wave, fidcosmo
+        )
+        k, fourier_profile = fft_profile(
+            profile_comov_dist, np.abs(comov_dist[1] - comov_dist[0])
+        )
 
         integrand[i] = fourier_profile * mean_density * cddf_NHI[i]
 
@@ -137,5 +182,5 @@ def main():
     np.savetxt(args.output, np.c_[k, Fvoigt])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
