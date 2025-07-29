2127901aae1dd4613e0e4a63302ee13024045d28
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2025-07-29 11:18:23 +0200

    fixed zero error in iminuit

--- py/picca/delta_extraction/expected_fluxes/utils.py
+++ py/picca/delta_extraction/expected_fluxes/utils.py
@@ -99,8 +99,8 @@ def compute_continuum(forest, get_mean_cont, get_eta, get_var_lss, get_fudge,
         minimizer = iminuit.Minuit(leasts_squares,
                                 zero_point=zero_point,
                                 slope=slope)
-        minimizer.errors["zero_point"] = np.fabs(zero_point) / 10.
-        minimizer.errors["slope"] = np.fabs(slope) / 10.
+        minimizer.errors["zero_point"] = np.max(np.fabs(zero_point) / 10., 1e-6)
+        minimizer.errors["slope"] = np.max(np.fabs(slope) / 10., 1e-6)
         minimizer.errordef = 1.
         minimizer.print_level = 0
         minimizer.fixed["slope"] = order == 0
